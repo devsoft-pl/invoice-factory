01ed4e515ce0035163bc90e5fdf39958a845051d
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2022-01-13 22:40:21 +0100

    update UserSerializer + def create

--- invoices/serializers.py
+++ invoices/serializers.py
@@ -1,4 +1,7 @@
 from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+from rest_framework.validators import UniqueValidator
+
 from invoices.models import (
     VatRate,
     Currency,
@@ -12,7 +15,19 @@ from rest_framework import serializers
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['url', 'username', 'email', 'groups']
+        fields = ['id', 'username', 'first_name', 'last_name', 'password', 'email']
+        extra_kwargs = {
+            'password': {'required': True, 'write_only': True},
+            'first_name': {'required': True},
+            'last_name': {'required': True},
+            'email': {'required': True, 'validators': [UniqueValidator(queryset=User.objects.all())]}
+        }
+
+        def create(self, validated_data):
+            """Create user nad token"""
+            user = User.objects.create_user(**validated_data)
+            Token.objects.create(user=user)
+            return user
 
 
 class VatRateSerializer(serializers.ModelSerializer):
