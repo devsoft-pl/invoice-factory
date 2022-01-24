dba4635acaba43a9e88888071eaa0850b7659f16
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2022-01-24 13:41:06 +0100

    update model VatRate - add user

--- invoices/models.py
+++ invoices/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
 from django.utils.translation import gettext as _
@@ -5,6 +6,7 @@ from django.utils.translation import gettext as _
 
 class VatRate(models.Model):
     rate = models.PositiveIntegerField(verbose_name=_('Rate'), unique=True)
+    user = models.ForeignKey(User, verbose_name=_('User'), on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return str(self.rate)
