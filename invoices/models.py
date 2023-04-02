7fb383427c6760ddf9f68b71596bb1d4af03b551
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-04-02 22:43:18 +0200

    black + isort

--- invoices/models.py
+++ invoices/models.py
@@ -166,14 +166,22 @@ class Invoice(models.Model):
 
 
 class Item(models.Model):
-    invoice = models.ForeignKey(Invoice, verbose_name=_("Invoice"), on_delete=models.CASCADE, related_name="items")
+    invoice = models.ForeignKey(
+        Invoice,
+        verbose_name=_("Invoice"),
+        on_delete=models.CASCADE,
+        related_name="items",
+    )
     name = models.CharField(verbose_name=_("Name"), max_length=255)
     pkwiu = models.CharField(verbose_name=_("PWKiU"), max_length=50, null=True)
     amount = models.PositiveIntegerField(verbose_name=_("Amount"))
     net_price = models.PositiveIntegerField(verbose_name=_("Net price"))
-    vat = models.ForeignKey(VatRate, verbose_name=_("Vat"), on_delete=models.CASCADE, related_name="item")
-    user = models.ForeignKey(User, verbose_name=_("User"), on_delete=models.CASCADE, null=True)
-
+    vat = models.ForeignKey(
+        VatRate, verbose_name=_("Vat"), on_delete=models.CASCADE, related_name="item"
+    )
+    user = models.ForeignKey(
+        User, verbose_name=_("User"), on_delete=models.CASCADE, null=True
+    )
 
     def __str__(self):
         return self.name
