65c45b1b3da7182f03adcd6e622e9b82c7601be0
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-04-04 21:16:53 +0200

    dodanie widoku invoices_view

--- invoices/views.py
+++ invoices/views.py
@@ -1,13 +1,38 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 from django.template.loader import get_template
 from xhtml2pdf import pisa
 
-
-def index_view(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+from invoices.models import Invoice
 
 
-def invoice_view(request):
+def invoices_view(request):
+    invoices = Invoice.objects.all()
+    context = {"invoices": invoices}
+    return render(request, "invoices.html", context)
+
+
+def detail_invoice_view(request):
+    pass
+
+
+def create_invoice_view():
+    pass
+
+
+def replace_invoice_view():
+    pass
+
+
+def update_invoice_view():
+    pass
+
+
+def delete_invoice_view():
+    pass
+
+
+def invoice_pdf_view(request):
     template_path = "invoice.html"
     context = {"myvar": "this is your template context"}
     # Create a Django response object, and specify content_type as pdf
