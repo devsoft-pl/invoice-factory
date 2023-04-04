10f57e318422d6ee6f8b788d952b501b7855d715
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-04-04 20:21:28 +0200

    dodanie nowego wywołania path do widoku

--- invoices/urls.py
+++ invoices/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 
-from invoices.views import invoice_view
+from invoices.views import index_view, invoice_view
 
 app_name = "invoices"
-urlpatterns = [path("invoice/", invoice_view, name="invoice_view")]
+urlpatterns = [
+    path("invoice/index", index_view, name="index"),
+    path("invoice/", invoice_view, name="invoice_view"),
+]
