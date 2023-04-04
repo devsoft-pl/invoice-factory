901478515c07583e1e9a27ba6ea136da155cea19
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-04-04 22:23:38 +0200

    dodanie formularza CurrencyForm

--- invoices/forms.py
+++ invoices/forms.py
@@ -1,9 +1,20 @@
 from django import forms
 
-from invoices.models import VatRate
+from invoices.models import (
+    VatRate,
+    Currency
+)
 
 
 class VatRateForm(forms.ModelForm):
     class Meta:
         model = VatRate
         fields = ["rate"]
+
+
+class CurrencyForm(forms.ModelForm):
+
+    class Meta:
+        model = Currency
+        fields = ["Currency"]
+
