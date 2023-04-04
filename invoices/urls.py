eaa52d83b759224fad078ffeec463d6f0daa6ba1
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-04-04 21:15:24 +0200

    refaktor urls

--- invoices/urls.py
+++ invoices/urls.py
@@ -1,9 +1,18 @@
 from django.urls import path
 
-from invoices.views import index_view, invoice_view
+from invoices.views import (create_invoice_view, delete_invoice_view,
+                            detail_invoice_view, invoice_pdf_view,
+                            invoices_view, replace_invoice_view,
+                            update_invoice_view)
 
 app_name = "invoices"
 urlpatterns = [
-    path("invoice/index", index_view, name="index"),
-    path("invoice/", invoice_view, name="invoice_view"),
+    path("invoices/", invoices_view, name="invoices"),
+    path("invoices/<int:invoice_id>/", detail_invoice_view, name="detail"),
+    path("invoices/create/", create_invoice_view, name="create"),
+    path("invoices/<int:invoice_id>/", replace_invoice_view, name="replace"),
+    path("invoices/<int:invoice_id>/", update_invoice_view, name="update"),
+    path("invoices/<int:invoice_id>/", delete_invoice_view, name="delete"),
+    path("invoices/pdf/", invoice_pdf_view, name="pdf"),
+    # path("invoice/", invoice_view, name="invoice_view")
 ]
