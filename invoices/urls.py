d6b8cd419ecdfd915d3c4f743754e0193a50777c
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-04-05 23:04:53 +0200

    dodanie nowego path z tworzeniem company

--- invoices/urls.py
+++ invoices/urls.py
@@ -1,19 +1,20 @@
 from django.urls import path
 
-from invoices.views import (create_invoice_view, delete_invoice_view,
-                            detail_invoice_view, index_view,
-                            list_invoices_view, pdf_invoice_view,
+from invoices.views import (create_company_view, create_invoice_view,
+                            delete_invoice_view, detail_invoice_view,
+                            index_view, list_invoices_view, pdf_invoice_view,
                             replace_invoice_view, update_invoice_view)
 
 app_name = "invoices"
 urlpatterns = [
     path("index/", index_view, name="index"),
-    path("", list_invoices_view, name="list"),
-    path("<int:invoice_id>/", detail_invoice_view, name="detail"),
-    path("create/", create_invoice_view, name="create"),
-    path("replace/<int:invoice_id>/", replace_invoice_view, name="replace"),
-    path("update/<int:invoice_id>/", update_invoice_view, name="update"),
-    path("delete/<int:invoice_id>/", delete_invoice_view, name="delete"),
-    path("pdf/", pdf_invoice_view, name="pdf"),
+    path("", list_invoices_view, name="list_invoices"),
+    path("<int:invoice_id>/", detail_invoice_view, name="detail_invoice"),
+    path("create/", create_invoice_view, name="create_invoice"),
+    path("replace/<int:invoice_id>/", replace_invoice_view, name="replace_invoice"),
+    path("update/<int:invoice_id>/", update_invoice_view, name="update_invoice"),
+    path("delete/<int:invoice_id>/", delete_invoice_view, name="delete_invoice"),
+    path("pdf/", pdf_invoice_view, name="pdf_invoice"),
+    path("company/create/", create_company_view, name="create_company"),
     # path("invoice/", invoice_view, name="invoice_view")
 ]
