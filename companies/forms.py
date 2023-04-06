20949ce91e773a1eba64f0167487f4941fc70ad4
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-04-06 21:21:41 +0200

    stworzenie forms dla aplikacji companies

---
+++ companies/forms.py
@@ -0,0 +1,39 @@
+from django import forms
+
+from companies.models import Company, Country
+
+
+class CountryForm(forms.ModelForm):
+    class Meta:
+        model = Country
+        fields = ["country"]
+
+
+class CompanyForm(forms.ModelForm):
+    next = forms.CharField(widget=forms.HiddenInput(), required=False)
+
+    class Meta:
+        model = Company
+        fields = [
+            "next",
+            "name",
+            "nip",
+            "regon",
+            "country",
+            "address",
+            "zip_code",
+            "city",
+            "email",
+            "phone_number",
+        ]
+        labels = {
+            "name": "Nazwa firmy",
+            "nip": "Nip",
+            "regon": "Regon",
+            "country": "Kraj",
+            "address": "Adres",
+            "zip_code": "Kod pocztowy",
+            "city": "Miasto",
+            "email": "Email",
+            "phone_number": "Numer telefonu",
+        }
