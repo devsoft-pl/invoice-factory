011cbb5f37af3f355c708f2bf27912874115b310
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-04-06 21:18:27 +0200

    dodanie modelu comapny i country

--- companies/models.py
+++ companies/models.py
@@ -1,3 +1,43 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils.translation import gettext as _
 
-# Create your models here.
+
+class Country(models.Model):
+    country = models.CharField(verbose_name=_("Country"), max_length=30, unique=True)
+    user = models.ForeignKey(
+        User, verbose_name=_("User"), on_delete=models.CASCADE, null=True
+    )
+
+    def __str__(self):
+        return self.country
+
+    class Meta:
+        verbose_name_plural = "countries"
+
+
+class Company(models.Model):
+    name = models.CharField(verbose_name=_("Name"), max_length=100)
+    nip = models.CharField(verbose_name=_("NIP"), max_length=12, unique=True)
+    regon = models.CharField(
+        verbose_name=_("Regon"), max_length=12, unique=True, null=True
+    )
+    country = models.ForeignKey(
+        Country, verbose_name=_("Country"), on_delete=models.CASCADE, null=True
+    )
+    address = models.CharField(verbose_name=_("Address"), max_length=100)
+    zip_code = models.CharField(verbose_name=_("ZIP Code"), max_length=10)
+    city = models.CharField(verbose_name=_("City"), max_length=60)
+    email = models.EmailField(verbose_name=_("Email"))
+    phone_number = models.CharField(
+        verbose_name=_("Phone number"), max_length=20, null=True, blank=True
+    )
+    user = models.ForeignKey(
+        User, verbose_name=_("User"), on_delete=models.CASCADE, null=True
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "companies"
