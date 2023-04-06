02123752a555c041786bc0217c3008f7fbcb8064
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-04-06 21:18:10 +0200

    dodanie modelu comapny i country do admin

--- companies/admin.py
+++ companies/admin.py
@@ -1,3 +1,50 @@
 from django.contrib import admin
 
-# Register your models here.
+from companies.models import Company, Country
+
+
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ("name", "nip", "email", "phone_number", "user")
+    search_fields = ("name", "nip", "regon", "user__username")
+    list_filter = ("user",)
+
+    fieldsets = (
+        (
+            "User info",
+            {
+                "fields": ("user",),
+            },
+        ),
+        (
+            "Basic information",
+            {
+                "fields": ("name", "nip", "regon"),
+            },
+        ),
+        (
+            "Address data",
+            {
+                "fields": (
+                    ("address",),
+                    ("zip_code", "city"),
+                    ("country",),
+                    ("email", "phone_number"),
+                )
+            },
+        ),
+    )
+
+
+@admin.register(Country)
+class CountryAdmin(admin.ModelAdmin):
+    list_display = ("country", "user")
+    list_filter = ("user",)
+    fieldsets = (
+        (
+            "Basic information",
+            {
+                "fields": ("country", "user"),
+            },
+        ),
+    )
