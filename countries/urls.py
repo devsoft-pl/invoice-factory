3356972498abe8122252c409a908eddddb988626
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-04-07 14:24:24 +0200

    dodanie path do edycji danych

--- countries/urls.py
+++ countries/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
 from countries.views import (create_country_view, detail_country_view,
-                             list_countries_view)
+                             list_countries_view, replace_country_view)
 
 app_name = "countries"
 urlpatterns = [
     path("", list_countries_view, name="list_countries"),
     path("<int:country_id>/", detail_country_view, name="detail_country"),
     path("create/", create_country_view, name="create_country"),
+    path("replace/<int:country_id>/", replace_country_view, name="replace_country"),
 ]
