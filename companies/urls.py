0df043a4d511aafc5656ea889be48da0d6a5416c
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-04-08 06:16:59 +0200

    dodanie path do edycji firmy

--- companies/urls.py
+++ companies/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
 from companies.views import (create_company_view, detail_company_view,
-                             list_companies_view)
+                             list_companies_view, replace_company_view)
 
 app_name = "companies"
 urlpatterns = [
     path("", list_companies_view, name="list_companies"),
     path("<int:company_id>/", detail_company_view, name="detail_company"),
     path("create/", create_company_view, name="create_company"),
+    path("replace/<int:company_id>/", replace_company_view, name="replace_company"),
 ]
