ac71fb25ac56b44ed71d1dcfd398c857ab5bf537
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-04-13 13:38:41 +0200

    dodanie przekierowan dla zalogowania, wylogowania i rejestracji user

---
+++ users/urls.py
@@ -0,0 +1,9 @@
+from django.urls import include, path
+
+from users.views import register_user_view
+
+app_name = "users"
+urlpatterns = [
+    path("", include("django.contrib.auth.urls")),
+    path("register/", register_user_view, name="register_user"),
+]
