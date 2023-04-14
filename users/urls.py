82f552a2f420787110694fd934a4ba45617db35e
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-04-14 11:41:10 +0200

    dodanie nowego path z detail user

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,13 @@
 from django.urls import include, path
 
-from users.views import register_user_view
+from users.views import (
+    register_user_view,
+    detail_user_view
+)
 
 app_name = "users"
 urlpatterns = [
     path("", include("django.contrib.auth.urls")),
     path("register/", register_user_view, name="register_user"),
+    path("<int:user_id>/", detail_user_view, name="detail_user"),
 ]
