aee02857fc7fcb9b8b788d86b8783cd323a29c87
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-04-16 19:45:39 +0200

    poprawa importów

--- base/urls_api.py
+++ base/urls_api.py
@@ -1,16 +1,22 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from invoices import views_api
+from companies.views_api import CompanyViewSet
+from countries.views_api import CountryViewSet
+from currencies.views_api import CurrencyViewSet
+from invoices.views_api import InvoiceViewSet
+from items.views_api import ItemViewSet
+from users.views_api import UserViewSet
+from vat_rates.views_api import VatRateViewSet
 
 router = routers.DefaultRouter()
-router.register(r"users", views_api.UserViewSet)
-router.register(r"vat_rates", views_api.VatRateViewSet)
-router.register(r"currencies", views_api.CurrencyViewSet)
-router.register(r"countries", views_api.CountryViewSet)
-router.register(r"companies", views_api.CompanyViewSet)
-router.register(r"invoices", views_api.InvoiceViewSet)
-router.register(r"items", views_api.ItemViewSet)
+router.register(r"users", UserViewSet)
+router.register(r"vat_rates", VatRateViewSet)
+router.register(r"currencies", CurrencyViewSet)
+router.register(r"countries", CountryViewSet)
+router.register(r"companies", CompanyViewSet)
+router.register(r"invoices", InvoiceViewSet)
+router.register(r"items", ItemViewSet)
 
 
 urlpatterns = [
