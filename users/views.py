e9541ea039aa130963ba4101f1f9b43067d3d626
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-04-16 20:45:52 +0200

    poprawka importu + update redirect

--- users/views.py
+++ users/views.py
@@ -1,11 +1,12 @@
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.http import Http404
 from django.shortcuts import get_object_or_404, redirect, render
 
 from companies.models import Company
+from users.forms import UserForm
 
 
 def register_user_view(request):
@@ -43,13 +44,13 @@ def replace_user_view(request, user_id):
         raise Http404("User does not exist")
 
     if request.method != "POST":
-        form = UserChangeForm(instance=user)
+        form = UserForm(instance=user)
     else:
-        form = UserChangeForm(instance=user, data=request.POST)
+        form = UserForm(instance=user, data=request.POST)
         if form.is_valid():
             form.save()
 
-            return redirect("users:detail_user")
+            return redirect("users:detail_user", request.user.pk)
 
     context = {"user": user, "form": form}
     return render(request, "registration/replace_user.html", context)
