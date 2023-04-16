db79aa60cdfcf38fab86c0161a63fa3b2d79bb7b
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-04-16 22:22:55 +0200

    dodanie hasła hashowanego

--- users/forms.py
+++ users/forms.py
@@ -1,8 +1,14 @@
 from django import forms
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
 from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
 
 
 class UserForm(forms.ModelForm):
+    password = ReadOnlyPasswordHashField(
+        label=_("password"),
+        help_text=_("Hasło jest szyfrowane, więc nie można je zobaczyć."),
+    )
     class Meta:
         model = User
         fields = ["username", "password", "email"]
