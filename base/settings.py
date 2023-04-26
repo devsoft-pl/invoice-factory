52eedf68e32e3bf746560f5a8cfbfe997db691ad
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-04-26 19:35:07 +0200

    podpięcie static i media do aplikacji

--- base/settings.py
+++ base/settings.py
@@ -126,11 +126,11 @@ THOUSAND_SEPARATOR = "$"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-MEDIA_URL = "media/"
-MEDIA_ROOT = "media"
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- base/urls.py
+++ base/urls.py
@@ -6,15 +6,19 @@ from rest_framework.authtoken.views import obtain_auth_token
 
 from base import urls_api
 
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("api/", include(urls_api)),
-    path("api-token-auth/", obtain_auth_token),
-    path("companies/", include("companies.urls")),
-    path("countries/", include("countries.urls")),
-    path("currencies/", include("currencies.urls")),
-    path("invoices/", include("invoices.urls")),
-    path("items/", include("items.urls")),
-    path("users/", include("users.urls")),
-    path("vat_rates/", include("vat_rates.urls")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns = (
+    [
+        path("admin/", admin.site.urls),
+        path("api/", include(urls_api)),
+        path("api-token-auth/", obtain_auth_token),
+        path("companies/", include("companies.urls")),
+        path("countries/", include("countries.urls")),
+        path("currencies/", include("currencies.urls")),
+        path("invoices/", include("invoices.urls")),
+        path("items/", include("items.urls")),
+        path("users/", include("users.urls")),
+        path("vat_rates/", include("vat_rates.urls")),
+    ]
+    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+)
