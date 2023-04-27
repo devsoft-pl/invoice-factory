1e83d9963769cd42f89c024937847516ffa2df58
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-04-27 20:33:50 +0200

    dodanie dodatkowego path do tworzenia wlasnego kraju uzytkownika

--- countries/urls.py
+++ countries/urls.py
@@ -9,6 +9,12 @@ urlpatterns = [
     path("", list_countries_view, name="list_countries"),
     path("<int:country_id>/", detail_country_view, name="detail_country"),
     path("create/", create_country_view, name="create_country"),
+    path(
+        "create_my_country/",
+        create_country_view,
+        name="create_my_country",
+        kwargs={"create_my_ountry": True},
+    ),
     path("replace/<int:country_id>/", replace_country_view, name="replace_country"),
     path("delete/<int:country_id>/", delete_country_view, name="delete_country"),
 ]
