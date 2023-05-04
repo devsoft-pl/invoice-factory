66b4faa5a0fa3caa470d7ffa99b468f182d0067e
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-05-04 17:03:14 +0200

    dodanie forms z filtrowaniem company

--- companies/forms.py
+++ companies/forms.py
@@ -21,18 +21,13 @@ class CompanyForm(forms.ModelForm):
             "email",
             "phone_number",
         ]
-        # labels = {
-        #     "name": "Nazwa firmy",
-        #     "nip": "Nip",
-        #     "regon": "Regon",
-        #     "country": "Kraj",
-        #     "address": "Adres",
-        #     "zip_code": "Kod pocztowy",
-        #     "city": "Miasto",
-        #     "email": "Email",
-        #     "phone_number": "Numer telefonu",
-        # }
 
     def __init__(self, *args, current_user, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields["country"].queryset = Country.objects.filter(user=current_user)
+
+
+class CompanyFilterForm(forms.Form):
+    name = forms.CharField(required=False)
+    nip = forms.CharField(required=False)
+    regon = forms.CharField(required=False)
