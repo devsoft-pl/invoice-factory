021cf16ae99bbcf5ca50a20147217f1195776b44
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-05-05 12:25:10 +0200

    dodanie path z zwianą hasła usera

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,15 @@
 from django.urls import include, path
 
-from users.views import detail_user_view, register_user_view, replace_user_view
+from users.views import (detail_user_view, password_change_user_view,
+                         register_user_view, replace_user_view)
 
 app_name = "users"
 urlpatterns = [
     path("", include("django.contrib.auth.urls")),
     path("register/", register_user_view, name="register_user"),
+    path(
+        "password_change_user/", password_change_user_view, name="password_change_user"
+    ),
     path("<int:user_id>/", detail_user_view, name="detail_user"),
     path("replace/<int:user_id>/", replace_user_view, name="replace_user"),
 ]
