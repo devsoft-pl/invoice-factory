2d7e3ed70c5422a45e18e74f2dbc9304d899be2c
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-05-10 22:12:39 +0200

    test sprawdzajacy zwracanie kodu kraju

---
+++ currencies/tests/test_models.py
@@ -0,0 +1,12 @@
+import pytest
+from currencies.models import Currency
+
+@pytest.mark.django_db
+class TestCurrencyModel:
+
+    @pytest.fixture(autouse=True)
+    def set_up(self) -> None:
+        self.currency = Currency.objects.create(code="pln")
+
+    def test_str_returns_currency_code(self):
+        assert self.currency.__str__() == self.currency.code
