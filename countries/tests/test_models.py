d9fc9f78212b5f77d841881fddd7544eb52b2a02
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-05-10 22:02:00 +0200

    zmiana nazwy zmiennej

--- countries/tests/test_models.py
+++ countries/tests/test_models.py
@@ -7,7 +7,7 @@ from countries.models import Country
 class TestCountryModels:
     @pytest.fixture(autouse=True)
     def set_up(self) -> None:
-        self.country_1 = Country.objects.create(country="pln")
+        self.country = Country.objects.create(country="pln")
 
     def test_str_returns_country_name(self):
-        assert self.country_1.__str__() == self.country_1.country
+        assert self.country.__str__() == self.country.country
