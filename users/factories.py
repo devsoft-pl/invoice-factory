bbb0445e5145780a272905445659dbab27216dbc
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-05-15 23:00:49 +0200

    factorka dla modelu user

---
+++ users/factories.py
@@ -0,0 +1,10 @@
+import factory
+from django.contrib.auth.models import User
+
+
+class UserFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = User
+
+    username = factory.Sequence(lambda n: "User %03d" % n)
+    email = factory.Sequence(lambda n: "test_%03d@test.pl" % n)
