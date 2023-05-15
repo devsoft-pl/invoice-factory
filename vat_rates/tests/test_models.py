2ff7768b80469ceb576f6525198c569dc973e98d
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-05-15 23:13:23 +0200

    update testu o factorke

--- vat_rates/tests/test_models.py
+++ vat_rates/tests/test_models.py
@@ -1,13 +1,13 @@
 import pytest
 
-from vat_rates.models import VatRate
+from vat_rates.factories import VatRateFactory
 
 
 @pytest.mark.django_db
 class TestVatRateModel:
     @pytest.fixture(autouse=True)
     def set_up(self) -> None:
-        self.vat_rate = VatRate.objects.create(rate="23")
+        self.vat_rate = VatRateFactory.create()
 
     def test_str_returns_vat_rate(self):
-        assert self.vat_rate.__str__() == self.vat_rate.rate
+        assert self.vat_rate.__str__() == str(self.vat_rate.rate)
