5ccd5b83f3320199d93da65f6d2ad7d8ce55b7e8
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-05-15 22:12:38 +0200

    poprawa testu o factorke

--- currencies/tests/test_models.py
+++ currencies/tests/test_models.py
@@ -1,13 +1,13 @@
 import pytest
 
-from currencies.models import Currency
+from currencies.factories import CurrencyFactory
 
 
 @pytest.mark.django_db
 class TestCurrencyModel:
     @pytest.fixture(autouse=True)
     def set_up(self) -> None:
-        self.currency = Currency.objects.create(code="pln")
+        self.currency = CurrencyFactory.create()
 
     def test_str_returns_currency_code(self):
         assert self.currency.__str__() == self.currency.code
