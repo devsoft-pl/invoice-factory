17503346980c9dca3ecdf667e8dbeaead328f1a8
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-05-15 22:59:59 +0200

    update factorki

--- currencies/factories.py
+++ currencies/factories.py
@@ -7,4 +7,4 @@ class CurrencyFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Currency
 
-    code = factory.Sequence(lambda n: "Kod %03d" % n)
+    code = factory.Sequence(lambda n: "Code %03d" % n)
