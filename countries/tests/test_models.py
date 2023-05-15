e6bbd778c0015f208fbda7c7c948079cee5fad17
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-05-15 22:04:55 +0200

    poprawka testu o faktorkę

--- countries/tests/test_models.py
+++ countries/tests/test_models.py
@@ -1,13 +1,13 @@
 import pytest
 
-from countries.models import Country
+from countries.factories import CountryFactory
 
 
 @pytest.mark.django_db
 class TestCountryModel:
     @pytest.fixture(autouse=True)
     def set_up(self) -> None:
-        self.country = Country.objects.create(country="Polska")
+        self.country = CountryFactory.create()
 
     def test_str_returns_country_name(self):
         assert self.country.__str__() == self.country.country
