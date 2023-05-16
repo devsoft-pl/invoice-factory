8b18aaee603cb88074a23102c6faa79c0195287e
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-05-16 22:35:42 +0200

    refaktor testow dla modelu invoice

--- invoices/tests/test_models.py
+++ invoices/tests/test_models.py
@@ -4,6 +4,7 @@ import pytest
 
 from invoices.factories import InvoiceFactory
 from items.factories import ItemFactory
+from vat_rates.factories import VatRateFactory
 
 
 @pytest.mark.django_db
@@ -12,32 +13,31 @@ class TestInvoiceModel:
     def set_up(self):
         self.invoice = InvoiceFactory.create()
         self.invoice_2 = InvoiceFactory.create()
-        self.item = ItemFactory.create(invoice=self.invoice)
-        self.item_2 = ItemFactory.create(invoice=self.invoice)
+        vat = VatRateFactory.create(rate=23)
+        self.item = ItemFactory.create(
+            invoice=self.invoice, amount=2, net_price=1200, vat=vat
+        )
+        self.item_2 = ItemFactory.create(
+            invoice=self.invoice, amount=1, net_price=800, vat=vat
+        )
 
     def test_str_returns_item_name(self):
         assert self.invoice.__str__() == self.invoice.invoice_number
 
     def test_returns_net_sum(self):
-        net_amount_1 = self.item.amount * self.item.net_price
-        nut_amount_2 = self.item_2.amount * self.item_2.net_price
-        assert self.invoice.net_amount == net_amount_1 + nut_amount_2
+        assert self.invoice.net_amount == Decimal("3200")
 
     def test_returns_zero_net_sum_if_no_items(self):
         assert self.invoice_2.net_amount == Decimal("0")
 
-    def test_returns_tax_amount_sum(self):
-        tax_amount_1 = (self.item.net_amount * self.item.vat.rate) / 100
-        tax_amount_2 = (self.item_2.net_amount * self.item_2.vat.rate) / 100
-        assert self.invoice.tax_amount == tax_amount_1 + tax_amount_2
+    def test_returns_tax_amount(self):
+        assert self.invoice.tax_amount == Decimal("736")
 
     def test_returns_zero_tax_amount_if_no_items(self):
         assert self.invoice_2.tax_amount == Decimal("0")
 
-    def test_returns_gross_amount_sum(self):
-        gross_amount_1 = Decimal(self.item.net_amount + self.item.tax_amount)
-        gross_amount_2 = Decimal(self.item_2.net_amount + self.item_2.tax_amount)
-        assert self.invoice.gross_amount == gross_amount_1 + gross_amount_2
+    def test_returns_gross_amount(self):
+        assert self.invoice.gross_amount == Decimal("3936")
 
     def test_returns_zero_gross_amount_if_no_items(self):
         assert self.invoice_2.gross_amount == Decimal("0")
