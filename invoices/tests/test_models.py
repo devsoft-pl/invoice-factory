0a4072a061d96d770f48f0b4a81789df293af726
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-05-16 21:25:25 +0200

    test sprawdzający zwracanie sumy netto z items w fakturze

--- invoices/tests/test_models.py
+++ invoices/tests/test_models.py
@@ -1,6 +1,7 @@
 import pytest
 
 from invoices.factories import InvoiceFactory
+from items.factories import ItemFactory
 
 
 @pytest.mark.django_db
@@ -8,6 +9,13 @@ class TestInvoiceModel:
     @pytest.fixture(autouse=True)
     def set_up(self):
         self.invoice = InvoiceFactory.create()
+        self.item = ItemFactory.create(invoice=self.invoice)
+        self.item_2 = ItemFactory.create(invoice=self.invoice)
 
     def test_str_returns_item_name(self):
         assert self.invoice.__str__() == self.invoice.invoice_number
+
+    def test_returns_net_sum(self):
+        net_amount_1 = self.item.amount * self.item.net_price
+        nut_amount_2 = self.item_2.amount * self.item_2.net_price
+        assert self.invoice.net_amount == net_amount_1 + nut_amount_2
