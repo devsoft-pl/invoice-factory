4020c9e31710e4115a75da4011fb3a3306a92c20
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-05-16 13:52:18 +0200

    update factorki

--- invoices/factories.py
+++ invoices/factories.py
@@ -2,7 +2,6 @@ import datetime
 
 import factory
 from factory import fuzzy
-from factory.fuzzy import FuzzyInteger
 
 from companies.factories import ClientCompanyFactory, MyCompanyFactory
 from currencies.factories import CurrencyFactory
@@ -15,14 +14,18 @@ class InvoiceFactory(factory.django.DjangoModelFactory):
         model = Invoice
 
     invoice_number = factory.Sequence(lambda n: "Invoice number %03d" % n)
-    invoice_type = FuzzyInteger(0, 1)
+    invoice_type = factory.fuzzy.FuzzyChoice(
+        Invoice.INVOICE_TYPES, getter=lambda i: i[0]
+    )
     company = factory.SubFactory(MyCompanyFactory)
     create_date = fuzzy.FuzzyDate(datetime.date(2023, 1, 1))
     sale_date = fuzzy.FuzzyDate(datetime.date(2023, 1, 1))
     payment_date = factory.Faker(
         "date_between_dates", date_start=factory.SelfAttribute("..sale_date")
     )
-    payment_method = FuzzyInteger(0, 1)
+    payment_method = factory.fuzzy.FuzzyChoice(
+        Invoice.PAYMENT_METHOD, getter=lambda p: p[0]
+    )
     currency = factory.SubFactory(CurrencyFactory)
     account_number = factory.Sequence(lambda n: "Account number %03d" % n)
     client = factory.SubFactory(ClientCompanyFactory)
