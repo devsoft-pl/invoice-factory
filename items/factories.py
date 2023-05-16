679211c65769d2d8c01a1cf63c11c4a711f72fea
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-05-16 13:52:34 +0200

    factorka dla modelu item

---
+++ items/factories.py
@@ -0,0 +1,20 @@
+import factory
+from factory import fuzzy
+
+from invoices.factories import InvoiceFactory
+from items.models import Item
+from users.factories import UserFactory
+from vat_rates.factories import VatRateFactory
+
+
+class ItemFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Item
+
+    invoice = factory.SubFactory(InvoiceFactory)
+    name = factory.Sequence(lambda n: "Name %03d" % n)
+    pkwiu = factory.Sequence(lambda n: "PKWIU %03d" % n)
+    amount = fuzzy.FuzzyInteger(1, 5)
+    net_price = fuzzy.FuzzyDecimal(100, 10000)
+    vat = factory.SubFactory(VatRateFactory)
+    user = factory.SubFactory(UserFactory)
