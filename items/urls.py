d357fbf89d4cf4bbe6227bccd5a81128671860be
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-05-18 00:04:02 +0200

    usuniecie z path list_items oraz detail_item

--- items/urls.py
+++ items/urls.py
@@ -1,19 +1,10 @@
 from django.urls import path
 
-from items.views import (create_item_view, delete_item_view, detail_item_view,
-                         list_items_view, replace_item_view)
+from items.views import create_item_view, delete_item_view, replace_item_view
 
 app_name = "items"
 urlpatterns = [
-    path("", list_items_view, name="list_items"),
-    path("<int:item_id>/", detail_item_view, name="detail_item"),
-    path("create/", create_item_view, name="create_item"),
-    path(
-        "create_my_item/",
-        create_item_view,
-        name="create_my_item",
-        kwargs={"create_my_item": True},
-    ),
+    path("create/<int:invoice_id>/", create_item_view, name="create_item"),
     path("replace/<int:item_id>/", replace_item_view, name="replace_item"),
     path("delete/<int:item_id>/", delete_item_view, name="delete_item"),
 ]
