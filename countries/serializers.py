3b23491b8428049d2ac737dc065c32430c955a81
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-05-17 11:50:11 +0200

    dodanie do serializera brakujacego pola

--- countries/serializers.py
+++ countries/serializers.py
@@ -6,7 +6,7 @@ from countries.models import Country
 class CountrySerializer(serializers.ModelSerializer):
     class Meta:
         model = Country
-        fields = ("id", "country", "user")
+        fields = ("id", "country", "user", "is_my_country")
         extra_kwargs = {
             "code": {"validators": []},
         }
