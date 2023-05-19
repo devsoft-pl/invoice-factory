6de9cfbe9756c899e87e473d2294a6e995c40536
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-05-20 00:21:33 +0200

    refactor nazw zmiennych w testach

--- companies/tests/test_model.py
+++ companies/tests/test_model.py
@@ -7,7 +7,7 @@ from companies.factories import CompanyFactory
 class TestCompanyModel:
     @pytest.fixture(autouse=True)
     def set_up(self) -> None:
-        self.my_company = CompanyFactory.create(is_my_company=True)
+        self.company = CompanyFactory.create(is_my_company=True)
 
     def test_str_returns_company_name(self):
-        assert self.my_company.__str__() == self.my_company.name
+        assert self.company.__str__() == self.company.name
