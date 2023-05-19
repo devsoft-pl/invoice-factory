778ac96f651b852e6e1d790063e5a25d1bf508cf
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-05-19 19:53:18 +0200

    testy sprawdzajace wyszukiwanie firm po nip i regon

--- companies/tests/test_forms.py
+++ companies/tests/test_forms.py
@@ -11,8 +11,12 @@ class TestCompanyForm:
     @pytest.fixture(autouse=True)
     def set_up(self) -> None:
         self.user = UserFactory.create()
-        self.company_1 = CompanyFactory.create(name="Devsoft", user=self.user)
-        self.company_2 = CompanyFactory.create(name="Microsoft", user=self.user)
+        self.company_1 = CompanyFactory.create(
+            name="Devsoft", nip="1111111111", regon="1111111", user=self.user
+        )
+        self.company_2 = CompanyFactory.create(
+            name="Microsoft", nip="2222222222", regon="2222222", user=self.user
+        )
 
     def test_return_company_with_name_startswith(self):
         request_get = {"name": "Dev"}
@@ -47,3 +51,21 @@ class TestCompanyForm:
         companies_list = Company.my_clients.filter(user=self.user)
         companies_list = self.form.get_filtered_companies(companies_list)
         assert len(companies_list) == 0
+
+    def test_return_company_with_nip(self):
+        request_get = {"nip": "1111111111"}
+        self.form = CompanyFilterForm(request_get)
+        self.form.is_valid()
+        companies_list = Company.my_clients.filter(user=self.user)
+        companies_list = self.form.get_filtered_companies(companies_list)
+        assert self.company_1.id == companies_list[0].id
+        assert len(companies_list) == 1
+
+    def test_return_company_with_regon(self):
+        request_get = {"regon": "1111111"}
+        self.form = CompanyFilterForm(request_get)
+        self.form.is_valid()
+        companies_list = Company.my_clients.filter(user=self.user)
+        companies_list = self.form.get_filtered_companies(companies_list)
+        assert self.company_1.id == companies_list[0].id
+        assert len(companies_list) == 1
