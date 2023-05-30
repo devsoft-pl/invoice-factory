f2bf46526bb0663bd4a752e18febaf254019c747
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-05-30 22:09:18 +0200

    testy dla widoku szczegolu firmy

--- companies/tests/test_views.py
+++ companies/tests/test_views.py
@@ -11,13 +11,14 @@ class TestCompany(TestCase):
         self.user.set_password("test")
         self.user.save()
         self.user_companies = CompanyFactory.create_batch(12, user=self.user)
-        self.user_company = CompanyFactory()
+        self.other_company = CompanyFactory()
+
 
 class TestListCompanies(TestCompany):
     def test_list_companies_if_not_logged(self):
         url = reverse("companies:list_companies")
         response = self.client.get(url, follow=True)
-        self.assertRedirects(response, "/users/login/?next=/companies/")
+        self.assertRedirects(response, f"/users/login/?next={url}")
 
     def test_list_companies_if_logged(self):
         url = reverse("companies:list_companies")
@@ -40,3 +41,27 @@ class TestListCompanies(TestCompany):
 
         self.assertTrue(len(object_list) == 2)
         self.assertListEqual(list(object_list), self.user_companies[10:])
+
+
+class TestDetailCompany(TestCompany):
+    def test_detail_company_if_not_logged(self):
+        url = reverse("companies:detail_company", args=[self.user_companies[0].pk])
+        response = self.client.get(url, follow=True)
+        self.assertRedirects(response, f"/users/login/?next={url}")
+
+    def test_detail_company_if_logged(self):
+        company = self.user_companies[0]
+        url = reverse("companies:detail_company", args=[company.pk])
+        self.client.login(username=self.user.username, password="test")
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "companies/detail_company.html")
+        self.assertEqual(company.pk, response.context["company"].pk)
+
+    def test_return_404_if_not_my_company(self):
+        company = self.other_company
+        url = reverse("companies:detail_company", args=[company.pk])
+        self.client.login(username=self.user.username, password="test")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
