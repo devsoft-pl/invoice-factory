b4736335e6449849fe23807c680ecffee85deffe
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-05-30 22:39:07 +0200

    refaktor

--- invoices/tests/test_views.py
+++ invoices/tests/test_views.py
@@ -14,17 +14,21 @@ class TestInvoice(TestCase):
             InvoiceFactory.create_batch(12, user=self.user),
             key=lambda invoice: invoice.sale_date,
         )
-        self.user_invoice = InvoiceFactory()
+        self.other_invoice = InvoiceFactory()
 
+
+class TestListInvoices(TestInvoice):
+
+    def setUp(self) -> None:
+        super().setUp()
+        self.url = reverse("invoices:list_invoices")
     def test_list_invoices_if_not_logged(self):
-        url = reverse("invoices:list_invoices")
-        response = self.client.get(url, follow=True)
-        self.assertRedirects(response, "/users/login/?next=/invoices/")
+        response = self.client.get(self.url, follow=True)
+        self.assertRedirects(response, f"/users/login/?next={self.url}")
 
     def test_list_invoices_if_logged(self):
-        url = reverse("invoices:list_invoices")
         self.client.login(username=self.user.username, password="test")
-        response = self.client.get(url)
+        response = self.client.get(self.url)
 
         object_list = response.context["invoices"]
 
@@ -34,11 +38,18 @@ class TestInvoice(TestCase):
         self.assertListEqual(list(object_list), self.user_invoices[:10])
 
     def test_list_invoices_second_pag(self):
-        url = reverse("invoices:list_invoices")
         self.client.login(username=self.user.username, password="test")
-        response = self.client.get(f"{url}?page=2")
+        response = self.client.get(f"{self.url}?page=2")
 
         object_list = response.context["invoices"]
 
         self.assertTrue(len(object_list) == 2)
         self.assertListEqual(list(object_list), self.user_invoices[10:])
+
+
+class TestDetailInvoice(TestInvoice):
+    def test_detail_invoice_if_not_logged(self):
+        invoice = self.user_invoices[0]
+        url = reverse("invoices:detail_invoice", args=[invoice.pk])
+        response = self.client.get(url, follow=True)
+        self.assertRedirects(response, f"/users/login/?next={url}")
