70da97b9cd9dcb3021f5ca6d0682f22b4cde9dd0
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-05-30 22:36:04 +0200

    refaktor

--- companies/tests/test_views.py
+++ companies/tests/test_views.py
@@ -15,15 +15,17 @@ class TestCompany(TestCase):
 
 
 class TestListCompanies(TestCompany):
+    def setUp(self) -> None:
+        super().setUp()
+        self.url = reverse("companies:list_companies")
+
     def test_list_companies_if_not_logged(self):
-        url = reverse("companies:list_companies")
-        response = self.client.get(url, follow=True)
-        self.assertRedirects(response, f"/users/login/?next={url}")
+        response = self.client.get(self.url, follow=True)
+        self.assertRedirects(response, f"/users/login/?next={self.url}")
 
     def test_list_companies_if_logged(self):
-        url = reverse("companies:list_companies")
         self.client.login(username=self.user.username, password="test")
-        response = self.client.get(url)
+        response = self.client.get(self.url)
 
         object_list = response.context["companies"]
 
@@ -33,9 +35,8 @@ class TestListCompanies(TestCompany):
         self.assertListEqual(list(object_list), self.user_companies[:10])
 
     def test_list_companies_second_pag(self):
-        url = reverse("companies:list_companies")
         self.client.login(username=self.user.username, password="test")
-        response = self.client.get(f"{url}?page=2")
+        response = self.client.get(f"{self.url}?page=2")
 
         object_list = response.context["companies"]
 
@@ -44,24 +45,25 @@ class TestListCompanies(TestCompany):
 
 
 class TestDetailCompany(TestCompany):
+    def setUp(self) -> None:
+        super().setUp()
+        self.company = self.user_companies[0]
+        self.url = reverse("companies:detail_company", args=[self.company.pk])
+
     def test_detail_company_if_not_logged(self):
-        url = reverse("companies:detail_company", args=[self.user_companies[0].pk])
-        response = self.client.get(url, follow=True)
-        self.assertRedirects(response, f"/users/login/?next={url}")
+        response = self.client.get(self.url, follow=True)
+        self.assertRedirects(response, f"/users/login/?next={self.url}")
 
     def test_detail_company_if_logged(self):
-        company = self.user_companies[0]
-        url = reverse("companies:detail_company", args=[company.pk])
         self.client.login(username=self.user.username, password="test")
-        response = self.client.get(url)
+        response = self.client.get(self.url)
 
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "companies/detail_company.html")
-        self.assertEqual(company.pk, response.context["company"].pk)
+        self.assertEqual(self.company.pk, response.context["company"].pk)
 
     def test_return_404_if_not_my_company(self):
-        company = self.other_company
-        url = reverse("companies:detail_company", args=[company.pk])
+        url = reverse("companies:detail_company", args=[self.other_company.pk])
         self.client.login(username=self.user.username, password="test")
         response = self.client.get(url)
         self.assertEqual(response.status_code, 404)
