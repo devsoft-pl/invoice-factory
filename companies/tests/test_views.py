09ebb63e44737e6a75972fbdfcde2f625d94daf8
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-05-30 21:30:39 +0200

    testy widoku dla listy firmy zmiana testow z pytest na unitest

--- companies/tests/test_views.py
+++ companies/tests/test_views.py
@@ -1,27 +1,42 @@
-import pytest
-from django.test import RequestFactory
+from django.test import TestCase
+from django.urls import reverse
 
 from companies.factories import CompanyFactory
-from companies.views import list_companies_view
+from countries.factories import CountryFactory
 from users.factories import UserFactory
 
 
-@pytest.mark.django_db
-class TestCompanyView:
-    @pytest.fixture(autouse=True)
-    def set_up(self) -> None:
-        self.rf = RequestFactory()
+class TestComapny(TestCase):
+    def setUp(self) -> None:
         self.user = UserFactory()
-        self.company = CompanyFactory.create(user=self.user)
+        self.user.set_password("test")
+        self.user.save()
+        self.user_companies = CompanyFactory.create_batch(12, user=self.user)
+        self.user_company = CompanyFactory()
 
-    def test_list_companies_view_returns_200(self):
-        request = self.rf.get("/companies/")
-        request.user = self.user
-        response = list_companies_view(request)
-        assert response.status_code == 200
+    def test_list_companies_if_not_logged(self):
+        url = reverse("companies:list_companies")
+        response = self.client.get(url, follow=True)
+        self.assertRedirects(response, "/users/login/?next=/companies/")
 
-    def test_list_companies_returns_company(self):
-        request = self.rf.get("/companies/")
-        request.user = self.user
-        response = list_companies_view(request)
-        assert self.company.name in str(response.content)
+    def test_list_companies_if_logged(self):
+        url = reverse("companies:list_companies")
+        self.client.login(username=self.user.username, password="test")
+        response = self.client.get(url)
+
+        object_list = response.context["companies"]
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "companies/list_companies.html")
+        self.assertTrue(len(object_list) == 10)
+        self.assertListEqual(list(object_list), self.user_companies[:10])
+
+    def test_list_companies_second_pag(self):
+        url = reverse("companies:list_companies")
+        self.client.login(username=self.user.username, password="test")
+        response = self.client.get(f"{url}?page=2")
+
+        object_list = response.context["companies"]
+
+        self.assertTrue(len(object_list) == 2)
+        self.assertListEqual(list(object_list), self.user_companies[10:])
