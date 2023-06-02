d977ebd802821725d3370d21b16538e9556b1b9c
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-06-02 13:10:18 +0200

    usuniecie z modelu nieuzwanego pola

--- currencies/models.py
+++ currencies/models.py
@@ -8,9 +8,6 @@ class Currency(models.Model):
     user = models.ForeignKey(
         User, verbose_name=_("User"), on_delete=models.CASCADE, null=True
     )
-    is_my_currency = models.BooleanField(
-        verbose_name=_("Is my currency"), default=False, editable=False
-    )
 
     def __str__(self):
         return self.code
