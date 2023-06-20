e5dc24a5a19fbb6314ce1c4584d82f44a0acb21b
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-06-20 18:14:18 +0200

    testy dla rejestracji usera

--- users/test/test_views.py
+++ users/test/test_views.py
@@ -1,5 +1,4 @@
 from django.contrib.auth.models import User
-from django.core.exceptions import ObjectDoesNotExist
 from django.test import TestCase
 from django.urls import reverse
 
@@ -64,3 +63,36 @@ class TestReplaceUser(TestUser):
         self.assertTrue(
             User.objects.filter(email="test@test.pl", pk=self.user.pk).exists()
         )
+
+
+class TestRegisterUser(TestUser):
+    def setUp(self) -> None:
+        super().setUp()
+        self.url = reverse("users:register_user")
+
+    def test_invalid_form_display_errors(self):
+        response = self.client.post(self.url, {})
+
+        self.assertEqual(response.status_code, 200)
+        self.assertFormError(
+            response.context["form"], "username", "To pole jest wymagane."
+        )
+        self.assertFormError(
+            response.context["form"], "password1", "To pole jest wymagane."
+        )
+        self.assertFormError(
+            response.context["form"], "password2", "To pole jest wymagane."
+        )
+        self.assertTemplateUsed(response, "registration/register.html")
+
+    def test_register_user_with_valid_data(self):
+        username = "User_test_1"
+        password = "Test_password1!"
+        response = self.client.post(
+            self.url,
+            {"username": username, "password1": password, "password2": password},
+        )
+
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse("invoices:index"))
+        self.assertTrue(User.objects.filter(username="User_test_1").exists())
