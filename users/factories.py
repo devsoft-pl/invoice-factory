1c727c2e00b8bca6d024347b046eea2aab2f893a
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-07-20 23:09:39 +0200

    dodanie factory dict dla user

--- users/factories.py
+++ users/factories.py
@@ -8,3 +8,9 @@ class UserFactory(factory.django.DjangoModelFactory):
 
     username = factory.Sequence(lambda n: "User%03d" % n)
     email = factory.Sequence(lambda n: "test_%03d@test.pl" % n)
+
+
+class UserDictFactory(factory.DictFactory):
+    username = factory.Sequence(lambda n: "user%03d" % n)
+    password = factory.Sequence(lambda n: "password%03d" % n)
+    email = factory.Sequence(lambda n: "test_%03d@test.pl" % n)
