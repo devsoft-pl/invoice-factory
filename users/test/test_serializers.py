2469df0e52408deea04dd50145eb5fa7b246b04e
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-07-20 23:11:05 +0200

    test serializera dla tworzenia usera

---
+++ users/test/test_serializers.py
@@ -0,0 +1,19 @@
+import pytest
+
+from users.factories import UserDictFactory
+from users.serializers import UserSerializer
+
+
+@pytest.mark.django_db
+class TestCreateUser:
+    @pytest.fixture(autouse=True)
+    def set_up(self):
+        self.user_data = UserDictFactory()
+
+    def test_create_user_if_valid_data(self):
+        serializer = UserSerializer()
+        user = serializer.create(self.user_data)
+
+        assert user.email == self.user_data["email"]
+        assert user.pk
+        assert user.auth_token
