0d79f7c83b4852e54c8d6cebd6c1d421ff560127
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-07-23 17:04:05 +0200

    testy dla api usera

--- users/views_api.py
+++ users/views_api.py
@@ -4,15 +4,6 @@ from rest_framework import viewsets
 from users.serializers import UserSerializer
 
 
-class OwnedObjectsMixin:
-    def get_queryset(self):
-        user = self.request.user
-        if user.is_superuser:
-            return super().get_queryset()
-        else:
-            return super().get_queryset().filter(user_id=user.id)
-
-
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
