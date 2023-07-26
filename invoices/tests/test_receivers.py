55d17769921e1d7ee4434ebdaceda374ae936445
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-07-26 21:56:58 +0200

    testy dla wysyłania maila powitalnego i pozegnalnego

--- invoices/tests/test_receivers.py
+++ invoices/tests/test_receivers.py
@@ -1,18 +1,48 @@
+from unittest.mock import patch
+
 import pytest
 
+from invoices.receivers import send_goodbye_email, send_welcome_email
 from users.factories import UserFactory
 
 
 @pytest.mark.django_db
 class TestSendEmail:
-
     @pytest.fixture(autouse=True)
     def set_up(self):
         self.user = UserFactory()
-        self.email_sender = "sender@test.pl"
-        self.subject = "Test tematu"
-        self.content = "Test treści"
+        self.user_without_mail = UserFactory(email="")
 
-    def test_send_welcome_message_when_user_with_email_is_created(self):
-        pass
+    @patch("invoices.receivers.send_mail")
+    def test_send_welcome_mail_when_user_with_email_is_created(self, send_mail_mock):
+        send_welcome_email(None, self.user, created=True)
 
+        send_mail_mock.assert_called_once()
+
+    @patch("invoices.receivers.send_mail")
+    def test_not_send_welcome_mail_when_is_not_created(self, send_mail_mock):
+        send_welcome_email(None, self.user, created=False)
+
+        send_mail_mock.assert_not_called()
+
+    @patch("invoices.receivers.send_mail")
+    def test_not_send_welcome_mail_when_user_without_email_is_created(
+        self, send_mail_mock
+    ):
+        send_welcome_email(None, self.user_without_mail, created=True)
+
+        send_mail_mock.assert_not_called()
+
+    @patch("invoices.receivers.send_mail")
+    def test_send_goodbye_mail_when_user_with_email_is_delete(self, send_mail_mock):
+        send_goodbye_email(None, self.user)
+
+        send_mail_mock.asser_calles_onece()
+
+    @patch("invoices.receivers.send_mail")
+    def test_not_send_goodbye_mail_when_user_without_email_is_delete(
+        self, send_mail_mock
+    ):
+        send_goodbye_email(None, self.user_without_mail)
+
+        send_mail_mock.assert_not_called()
