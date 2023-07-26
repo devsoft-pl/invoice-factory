be8e92026d0ddd5a4cc18b26c1bae1945b9893fc
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-07-26 20:18:33 +0200

    zmiana nadawcy email

--- invoices/receivers.py
+++ invoices/receivers.py
@@ -40,6 +40,6 @@ def send_goodbye_email(sender, instance: User, **kwargs):
         send_mail(
             subject,
             content,
-            from_email="wioletta.wajda82@gmail.com",
+            from_email="EMAIL_SENDER",
             recipient_list=[instance.email],
         )
