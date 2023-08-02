fc512c8d0c4496381f9448bc470aba71ac4f54e2
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-08-02 21:14:59 +0200

    refaktor testow dla adaptera

--- currencies/tests/test_nbp_adapter.py
+++ currencies/tests/test_nbp_adapter.py
@@ -9,10 +9,7 @@ class TestNBPExchangeRatesAdapter:
     @pytest.fixture(autouse=True)
     def set_up(self) -> None:
         self.adapter = NBPExchangeRatesAdapter()
-
-    @patch("currencies.nbp.adapter.NBPExchangeRatesAdapter._get_currency_rates")
-    def test_get_currency_buy_rate(self, _get_currency_rates_mock):
-        _get_currency_rates_mock.return_value = {
+        self.currency_rates_value = {
             "table": "C",
             "currency": "dolar amerykański",
             "code": "USD",
@@ -25,23 +22,15 @@ class TestNBPExchangeRatesAdapter:
                 }
             ],
         }
+
+    @patch("currencies.nbp.adapter.NBPExchangeRatesAdapter._get_currency_rates")
+    def test_get_currency_buy_rate(self, _get_currency_rates_mock):
+        _get_currency_rates_mock.return_value = self.currency_rates_value
 
         assert self.adapter.get_currency_buy_rate("USD") == 3.9758
 
     @patch("currencies.nbp.adapter.NBPExchangeRatesAdapter._get_currency_rates")
     def test_get_currency_sell_rate(self, _get_currency_rates_mock):
-        _get_currency_rates_mock.return_value = {
-            "table": "C",
-            "currency": "dolar amerykański",
-            "code": "USD",
-            "rates": [
-                {
-                    "no": "143/C/NBP/2023",
-                    "effectiveDate": "2023-07-26",
-                    "bid": 3.9758,
-                    "ask": 4.0562,
-                }
-            ],
-        }
+        _get_currency_rates_mock.return_value = self.currency_rates_value
 
         assert self.adapter.get_currency_sell_rate("USD") == 4.0562
