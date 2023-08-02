4144c1a1c5a5e20a14e4f6047a453f9b24e7179c
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-08-02 22:49:26 +0200

    test metody str dla modelu ExchenageRate

--- currencies/tests/test_models.py
+++ currencies/tests/test_models.py
@@ -1,6 +1,6 @@
 import pytest
 
-from currencies.factories import CurrencyFactory
+from currencies.factories import CurrencyFactory, ExchangeRateFactory
 
 
 @pytest.mark.django_db
@@ -11,3 +11,16 @@ class TestCurrencyModel:
 
     def test_str_returns_currency_code(self):
         assert self.currency.__str__() == self.currency.code
+
+
+@pytest.mark.django_db
+class TestExchangeRateModel:
+    @pytest.fixture(autouse=True)
+    def set_up(self) -> None:
+        self.exchange_rate = ExchangeRateFactory.create()
+
+    def test_str_returns_currency_code_with_date(self):
+        assert (
+            self.exchange_rate.__str__()
+            == f"{self.exchange_rate.currency.code}: {self.exchange_rate.date}"
+        )
