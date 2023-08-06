cff23dad5388220148b4fd20984a1b05384b32bb
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-08-06 17:55:07 +0200

    dodani sprawdzania warunków istnienia danych do metod z kupnem i sprzedaza waluty

--- currencies/nbp/adapter.py
+++ currencies/nbp/adapter.py
@@ -5,18 +5,24 @@ class NBPExchangeRatesAdapter:
     base_url = "http://api.nbp.pl/api/"
 
     def _get_currency_rates(self, currency_code):
-        url = f"{self.base_url}exchangerates/rates/c/{currency_code}/today/"
+        url = f"{self.base_url}exchangerates/rates/c/{currency_code}/"
         response = requests.get(url, headers={"Accept": "application/json"})
+        if not response.status_code == 200:
+            return None
 
         return response.json()
 
     def get_currency_buy_rate(self, currency_code):
         data = self._get_currency_rates(currency_code)
+        if not data:
+            return None
 
         return data["rates"][0]["bid"]
 
     def get_currency_sell_rate(self, currency_code):
         data = self._get_currency_rates(currency_code)
+        if not data:
+            return None
 
         return data["rates"][0]["ask"]
 
