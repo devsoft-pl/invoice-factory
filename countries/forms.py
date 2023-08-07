0e49e1ee10df808ca7036eacf5c10a32ac086c57
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-08-07 21:17:08 +0200

    update metody tworzacej kraj o unikalnosc kraju dla kazdego uzytwnika

--- countries/forms.py
+++ countries/forms.py
@@ -1,11 +1,24 @@
 from django import forms
 
 from countries.models import Country
+from django.utils.translation import gettext as _
 
 
 class CountryForm(forms.ModelForm):
     next = forms.CharField(widget=forms.HiddenInput(), required=False)
 
+    def __init__(self, user, *args, **kwargs):
+        self.user = user
+        super().__init__(*args, **kwargs)
+
     class Meta:
         model = Country
         fields = ["next", "country"]
+
+    def clean_country(self):
+        country = self.cleaned_data.get("country")
+        country = Country.objects.filter(country=country, user=self.user)
+
+        if country.exists():
+            raise forms.ValidationError(_("Country already exists"))
+
