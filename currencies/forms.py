a20895757f1a2d5e67c94725c105f61737f19233
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-08-07 19:51:25 +0200

    dodanie do forms walidacji unikatywalnosci waluty dla kazdego usera

--- currencies/forms.py
+++ currencies/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.utils.translation import gettext as _
 
 from currencies.models import Currency
 
@@ -6,6 +7,19 @@ from currencies.models import Currency
 class CurrencyForm(forms.ModelForm):
     next = forms.CharField(widget=forms.HiddenInput(), required=False)
 
+    def __init__(self, user, *args, **kwargs):
+        self.user = user
+        super().__init__(*args, **kwargs)
+
     class Meta:
         model = Currency
         fields = ["next", "code"]
+
+    def clean_code(self):
+        code = self.cleaned_data.get("code")
+
+        currency = Currency.objects.filter(code=code, user=self.user)
+        if currency.exists():
+            raise forms.ValidationError(_("Currency already exists"))
+
+        return code
