e162fc41419f3886e56d0cc27c827a61b11a5414
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-08-07 20:07:04 +0200

    dodanie do forms walidacji unikatywnosci dla kazdego usera

--- vat_rates/forms.py
+++ vat_rates/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.utils.translation import gettext as _
 
 from vat_rates.models import VatRate
 
@@ -6,6 +7,19 @@ from vat_rates.models import VatRate
 class VatRateForm(forms.ModelForm):
     next = forms.CharField(widget=forms.HiddenInput(), required=False)
 
+    def __init__(self, user, *args, **kwargs):
+        self.user = user
+        super().__init__(*args, **kwargs)
+
     class Meta:
         model = VatRate
         fields = ["next", "rate"]
+
+    def clean_rate(self):
+        rate = self.cleaned_data.get("rate")
+
+        rate = VatRate.objects.filter(rate=rate, user=self.user)
+        if rate.exists():
+            raise forms.ValidationError(_("Vat rate already exists"))
+
+        return rate
