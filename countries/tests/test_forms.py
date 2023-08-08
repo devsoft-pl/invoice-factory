6a729090fbcfc71c5d2f434fe94e44ad0188f333
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-08-08 23:33:04 +0200

    dodanie testow sprawdzajacych poprawnosc walidacji danych

---
+++ countries/tests/test_forms.py
@@ -0,0 +1,25 @@
+import pytest
+
+from countries.factories import CountryFactory, CountryDictFactory
+from countries.forms import CountryForm
+from users.factories import UserFactory
+
+
+@pytest.mark.django_db
+class TestCountryForm:
+    @pytest.fixture(autouse=True)
+    def set_up(self) -> None:
+        self.user = UserFactory.create()
+
+    def test_form_with_valid_data(self):
+        data = CountryDictFactory()
+        form = CountryForm(user=self.user, data=data)
+        assert form.is_valid()
+        assert form.errors == {}
+
+    def test_clean_country_returns_error(self):
+        country = CountryFactory.create(user=self.user)
+        data = CountryDictFactory(country=country.country)
+        form = CountryForm(user=self.user, data=data)
+        assert not form.is_valid()
+        assert form.errors == {'country': ['Country already exists']}
