d649e588e79010ea513bf55b1a5af95ac248041c
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-08-08 22:57:18 +0200

    testy sprawdzajace walidacje danych dla stawki vat

---
+++ vat_rates/tests/test_forms.py
@@ -0,0 +1,25 @@
+import pytest
+
+from users.factories import UserFactory
+from vat_rates.factories import VatRateDictFactory, VatRateFactory
+from vat_rates.forms import VatRateForm
+
+
+@pytest.mark.django_db
+class TestVatRateForm:
+    @pytest.fixture(autouse=True)
+    def set_up(self) -> None:
+        self.user = UserFactory.create()
+        self.vat_rate = VatRateFactory.create(user=self.user)
+
+    def test_form_with_valid_data(self):
+        data = VatRateDictFactory()
+        form = VatRateForm(user=self.user, data=data)
+        assert form.is_valid()
+        assert form.errors == {}
+
+    def test_clean_rate_returns_error(self):
+        data = VatRateDictFactory(rate=self.vat_rate.rate)
+        form = VatRateForm(user=self.user, data=data)
+        assert not form.is_valid()
+        assert form.errors == {"rate": ["Vat rate already exists"]}
