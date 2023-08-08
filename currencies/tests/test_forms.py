43d6017497a21e3fd5d30960a8bd2ba9b87ed306
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-08-09 00:00:02 +0200

    dodanie testow sprawdzajacych poprawnosc walidacji unikalnosci waluty

---
+++ currencies/tests/test_forms.py
@@ -0,0 +1,25 @@
+import pytest
+
+from currencies.factories import CurrencyDictFactory, CurrencyFactory
+from currencies.forms import CurrencyForm
+from users.factories import UserFactory
+
+
+@pytest.mark.django_db
+class TestCurrencyForm:
+    @pytest.fixture(autouse=True)
+    def set_up(self) -> None:
+        self.user = UserFactory.create()
+
+    def test_form_with_valid_data(self):
+        data = CurrencyDictFactory(code="eur")
+        form = CurrencyForm(user=self.user, data=data)
+        assert form.is_valid()
+        assert form.errors == {}
+
+    def test_clean_currency_returns_error(self):
+        currency = CurrencyFactory.create(user=self.user, code="pln")
+        data = CurrencyDictFactory(code=currency.code)
+        form = CurrencyForm(user=self.user, data=data)
+        assert not form.is_valid()
+        assert form.errors == {"code": ["Waluta już istnieje"]}
