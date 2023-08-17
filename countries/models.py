3e9e522c93be2a17daf5b52ec0d96434ebb4dcd2
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-08-18 00:39:37 +0200

    dodanie walidacji pola dla kraju tylko zapisanego w postaci liter

--- countries/models.py
+++ countries/models.py
@@ -1,10 +1,17 @@
 from django.contrib.auth.models import User
+from django.core.validators import RegexValidator
 from django.db import models
 from django.utils.translation import gettext as _
 
 
 class Country(models.Model):
-    country = models.CharField(verbose_name=_("Country"), max_length=30)
+    country = models.CharField(
+        verbose_name=_("Country"),
+        max_length=30,
+        validators=[
+            RegexValidator(r"^[a-zA-Z]*$", _("Enter the country in letters only"))
+        ],
+    )
     user = models.ForeignKey(
         User, verbose_name=_("User"), on_delete=models.CASCADE, null=True
     )
