364cc35e9650eb3f9171dc228a3fa20ccb527fff
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-08-17 18:15:45 +0200

    refaktor codyw testach dla tasku z wymiana walut

--- currencies/tests/test_tasks.py
+++ currencies/tests/test_tasks.py
@@ -19,62 +19,14 @@ class TestExchangeRatesTasks:
         self.buy_rate = "3.9758"
         self.sell_rate = "4.0562"
 
-    @patch("currencies.nbp.adapter.NBPExchangeRatesAdapter.get_currency_buy_rate")
-    @patch("currencies.nbp.adapter.NBPExchangeRatesAdapter.get_currency_sell_rate")
+    @patch("currencies.tasks.get_exchange_rate_for_currency.apply_async")
     def test_should_create_exchange_rates_when_buy_rate_and_sell_rate(
-        self, get_currency_buy_rate_mock, get_currency_sell_rate_mock
+        self, get_exchange_rate_for_currency_mock
     ):
         self.currency_eur = CurrencyFactory.create(user=self.user, code="eur")
 
-        get_currency_buy_rate_mock.return_value = self.buy_rate
-        get_currency_sell_rate_mock.return_value = self.sell_rate
-
-        ExchangeRateFactory(currency=self.currency_usd, date=datetime.today())
-
         get_exchange_rates_for_all()
-
-        assert get_currency_buy_rate_mock.call_count == 2
-        assert get_currency_sell_rate_mock.call_count == 2
-
-        assert get_currency_buy_rate_mock.call_args_list == [call("eur"), call("usd")]
-
-        assert (
-            ExchangeRate.objects.filter(
-                date=datetime.today(), currency=self.currency_usd
-            ).count()
-            == 1
-        )
-        assert (
-            ExchangeRate.objects.filter(
-                date=datetime.today(), currency=self.currency_eur
-            ).count()
-            == 1
-        )
-
-    @patch("currencies.nbp.adapter.NBPExchangeRatesAdapter.get_currency_buy_rate")
-    @patch("currencies.nbp.adapter.NBPExchangeRatesAdapter.get_currency_sell_rate")
-    def test_should_not_create_exchange_rates_when_no_buy_rate_and_no_sell_rate(
-        self, get_currency_buy_rate_mock, get_currency_sell_rate_mock
-    ):
-        self.currency_eur = CurrencyFactory.create(user=self.user, code="eur")
-
-        get_currency_buy_rate_mock.return_value = None
-        get_currency_sell_rate_mock.return_value = None
-
-        get_exchange_rates_for_all()
-
-        assert (
-            ExchangeRate.objects.filter(
-                date=datetime.today(), currency=self.currency_usd
-            ).count()
-            == 0
-        )
-        assert (
-            ExchangeRate.objects.filter(
-                date=datetime.today(), currency=self.currency_eur
-            ).count()
-            == 0
-        )
+        assert get_exchange_rate_for_currency_mock.call_count == 2
 
     @patch("currencies.nbp.adapter.NBPExchangeRatesAdapter.get_currency_buy_rate")
     @patch("currencies.nbp.adapter.NBPExchangeRatesAdapter.get_currency_sell_rate")
