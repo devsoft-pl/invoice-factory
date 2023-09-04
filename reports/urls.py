1b497155546d223926e844719c7181a22967af87
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-09-04 22:41:49 +0200

    usuniecie nieuzywanego kodu

--- reports/urls.py
+++ reports/urls.py
@@ -1,8 +1,4 @@
 from django.urls import path
 
-from reports.views import list_reports_view
-
 app_name = "reports"
-urlpatterns = [
-    path("", list_reports_view, name="list_reports"),
-]
+urlpatterns = []
--- reports/views.py
+++ reports/views.py
@@ -1,5 +1,1 @@
 from django.shortcuts import render
-
-
-def list_reports_view():
-    pass
