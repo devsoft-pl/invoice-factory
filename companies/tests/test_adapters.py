6191da6958a909b91d6f0a4ee3d5b76a11f97e73
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-09-04 22:19:07 +0200

    dodanie testow dla krs adaptera

--- companies/tests/test_adapters.py
+++ companies/tests/test_adapters.py
@@ -3,6 +3,7 @@ from unittest.mock import Mock, patch
 import pytest
 
 from companies.govs_adapters.ceidg_adapter import CEIDGAdapter
+from companies.govs_adapters.krs_adapter import KrsAdapter
 
 
 class TestCEIDGAdapter:
@@ -71,3 +72,57 @@ class TestCEIDGAdapter:
         requests_get_mock.return_value = Mock(status_code=400)
 
         assert not self.adapter.is_company_active("123456789")
+
+
+class TestKrsAdapter:
+    @pytest.fixture(autouse=True)
+    def set_up(self) -> None:
+        self.adapter = KrsAdapter()
+        self.krs = "0000012345"
+        self.company_data = {
+            "odpis": {
+                "rodzaj": "Aktualny",
+                "naglowekA": {"numerKRS": "0000012345", "stanPozycji": 1},
+                "dane": {
+                    "dzial1": {
+                        "danePodmiotu": {
+                            "identyfikatory": {"regon": "...", "nip": "123456789"}
+                        }
+                    }
+                },
+            }
+        }
+
+    @patch("companies.govs_adapters.krs_adapter.KrsAdapter._get_company_data")
+    def test_returns_company_nip(self, _get_company_data_mock):
+        _get_company_data_mock.return_value = self.company_data
+
+        assert (
+            self.adapter.get_nip(self.krs)
+            == self.company_data["odpis"]["dane"]["dzial1"]["danePodmiotu"][
+                "identyfikatory"
+            ]["nip"]
+        )
+
+    @patch("companies.govs_adapters.krs_adapter.KrsAdapter._get_company_data")
+    def test_returns_company_regon(self, _get_company_data_mock):
+        _get_company_data_mock.return_value = self.company_data
+
+        assert (
+            self.adapter.get_regon(self.krs)
+            == self.company_data["odpis"]["dane"]["dzial1"]["danePodmiotu"][
+                "identyfikatory"
+            ]["regon"]
+        )
+
+    @patch("companies.govs_adapters.krs_adapter.KrsAdapter._get_company_data")
+    def test_returns_none_when_not_data(self, _get_company_data_mock):
+        _get_company_data_mock.return_value = {}
+
+        assert not self.adapter.get_nip(self.krs)
+
+    @patch("companies.govs_adapters.krs_adapter.requests.get")
+    def test_get_company_data_not_returns_status_code_200(self, requests_get_mock):
+        requests_get_mock.return_value = Mock(status_code=400)
+
+        assert not self.adapter.get_nip(self.krs)
