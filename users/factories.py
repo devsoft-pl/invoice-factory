1ac6ab89528beabfcf87cef522b462f840c6f2de
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-09-05 22:53:06 +0200

    update faktorek

--- users/factories.py
+++ users/factories.py
@@ -7,11 +7,9 @@ class UserFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = User
 
-    username = factory.Sequence(lambda n: "User%03d" % n)
     email = factory.Sequence(lambda n: "test_%03d@test.pl" % n)
 
 
 class UserDictFactory(factory.DictFactory):
-    username = factory.Sequence(lambda n: "user%03d" % n)
     password = factory.Sequence(lambda n: "password%03d" % n)
     email = factory.Sequence(lambda n: "test_%03d@test.pl" % n)
