34809f9823792854533c3c21a2b7d38443356898
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-09-05 22:47:11 +0200

    utworzenie modelu User

--- users/models.py
+++ users/models.py
@@ -1,3 +1,35 @@
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from .managers import UserManager
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(_("Email"), unique=True)
+    first_name = models.CharField(_("First name"), max_length=75, null=True)
+    last_name = models.CharField(_("Last name"), max_length=75, null=True)
+    is_staff = models.BooleanField(
+        _("Staff status"),
+        default=False,
+        help_text=_("Designates whether the user can log into this admin site."),
+    )
+    is_active = models.BooleanField(
+        _("Active"),
+        default=True,
+        help_text=_(
+            "Designates whether this user should be treated as active. Unselect this instead of deleting accounts."
+        ),
+    )
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
+    EMAIL_FIELD = "email"
+
+    def get_full_name(self) -> str:
+        return f"{self.first_name} {self.last_name}"
+
+    def get_short_name(self) -> str:
+        return self.email
