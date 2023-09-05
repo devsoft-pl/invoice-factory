861a2756f6853fac45969def694692ff67859ed8
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-09-05 22:48:14 +0200

    zmiana formularza User plus zmiana importu modelu User

--- users/forms.py
+++ users/forms.py
@@ -1,22 +1,16 @@
 from django import forms
-from django.contrib.auth.forms import ReadOnlyPasswordHashField
-from django.contrib.auth.forms import UserCreationForm as BaseUserCreationForm
-from django.contrib.auth.models import User
-from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.forms import BaseUserCreationForm
+
+from users.models import User
 
 
 class UserForm(forms.ModelForm):
-    password = ReadOnlyPasswordHashField(
-        label=_("password"),
-        help_text=_("Hasło jest szyfrowane, więc nie można je zobaczyć."),
-    )
-
     class Meta:
         model = User
-        fields = ["username", "password", "email"]
+        fields = ["first_name", "last_name", "email", "password"]
 
 
 class UserCreationForm(BaseUserCreationForm):
     class Meta:
         model = User
-        fields = ["username", "email"]
+        fields = ["email", "password1", "password2"]
