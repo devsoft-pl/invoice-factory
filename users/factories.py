9b300e172d78f926cb5b56d8ca5d6ff47752daa7
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-09-09 13:55:56 +0200

    dodanie do faktorki dodatkowych pol: pierwsze imie i nazwisko

--- users/factories.py
+++ users/factories.py
@@ -8,6 +8,8 @@ class UserFactory(factory.django.DjangoModelFactory):
         model = User
 
     email = factory.Sequence(lambda n: "test_%03d@test.pl" % n)
+    first_name = factory.Sequence(lambda n: "Test imie%03d" % n)
+    last_name = factory.Sequence(lambda n: "Test nazwisko%03d" % n)
 
 
 class UserDictFactory(factory.DictFactory):
