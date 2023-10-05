ab4a8f05c34dfc79b978c0aae0bb6a52d16ab86b
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-10-05 20:34:48 +0200

    przeniesienie walidacji pol do formulerza

--- companies/models.py
+++ companies/models.py
@@ -9,58 +9,20 @@ from users.models import User
 
 class Company(models.Model):
     name = models.CharField(verbose_name=_("Name"), max_length=100)
-    nip = models.CharField(
-        verbose_name=_("NIP"),
-        max_length=13,
-        validators=[
-            RegexValidator(
-                r"^[0-9a-zA-Z]{8,16}$",
-                _(
-                    "Enter the tax ID without special characters with minimum 8 character"
-                ),
-            )
-        ],
-    )
-    regon = models.CharField(
-        verbose_name=_("Regon"),
-        max_length=14,
-        null=True,
-        validators=[
-            RegexValidator(
-                r"^([0-9]{9}|[0-9]{14})$",
-                _("Enter regon in numbers only with minimum 9 character"),
-            )
-        ],
-    )
+    nip = models.CharField(verbose_name=_("NIP"), max_length=13)
+    regon = models.CharField(verbose_name=_("Regon"), max_length=14, null=True,)
     country = models.ForeignKey(
         Country, verbose_name=_("Country"), on_delete=models.CASCADE, null=True
     )
     address = models.CharField(verbose_name=_("Address"), max_length=100)
-    zip_code = models.CharField(
-        verbose_name=_("ZIP Code"),
-        max_length=10,
-        validators=[
-            RegexValidator(
-                r"^[0-9]{2}-[0-9]{3}$", _("Zip code in numbers only in format xx-xxx")
-            )
-        ],
-    )
-    city = models.CharField(
-        verbose_name=_("City"),
-        max_length=60,
-        validators=[
-            RegexValidator(r"^[a-zA-Z ]+$", _("Enter the city in letters only"))
-        ],
-    )
+    zip_code = models.CharField(verbose_name=_("ZIP Code"), max_length=10)
+    city = models.CharField(verbose_name=_("City"), max_length=60)
     email = models.EmailField(verbose_name=_("Email"), null=True, blank=True)
     phone_number = models.CharField(
         verbose_name=_("Phone number"),
         max_length=20,
         null=True,
-        blank=True,
-        validators=[
-            RegexValidator(r"^[0-9]{9,}$", _("Enter phone number in numbers only"))
-        ],
+        blank=True
     )
     user = models.ForeignKey(
         User, verbose_name=_("User"), on_delete=models.CASCADE, null=True
