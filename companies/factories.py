9b7d8cd6ce86313569e7d132713b1a6eec55c5b6
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-10-05 20:37:54 +0200

    dodanie pola do CompanyDictFactory: is_my_company

--- companies/factories.py
+++ companies/factories.py
@@ -32,3 +32,4 @@ class CompanyDictFactory(factory.DictFactory):
     city = factory.Sequence(lambda n: "City %03d" % n)
     email = factory.Sequence(lambda n: "test_%03d@test.pl" % n)
     phone_number = factory.Sequence(lambda n: "Phone %03d" % n)
+    is_my_company = factory.fuzzy.FuzzyChoice([True, False])
