dc346be50e8cb54b40c7b8b8b62599d84ca4db8d
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-10-09 20:31:54 +0200

    update metody generujacej faktury cykliczne  aby nazwa załącznika była intuicyjna zalezna od numeru utworzonej faktury

--- invoices/tasks.py
+++ invoices/tasks.py
@@ -16,16 +16,14 @@ logger = logging.getLogger(__name__)
 def create_invoices_for_recurring():
     logger.info("Trying to create invoices recurring")
 
-    invoices = Invoice.objects.filter(pk=1)
-    # invoices = Invoice.objects.filter(is_recurring=True)
-    #
+    invoices = Invoice.objects.filter(is_recurring=True)
     date = datetime.today()
-    #
-    # month_range = calendar.monthrange(date.year, date.month)
-    # last_day = month_range[1]
-    #
-    # if last_day != date.day:
-    #     return
+
+    month_range = calendar.monthrange(date.year, date.month)
+    last_day = month_range[1]
+
+    if last_day != date.day:
+        return
 
     for invoice in invoices:
         payment_date = date + timedelta(
@@ -45,6 +43,7 @@ def create_invoices_for_recurring():
             account_number=invoice.account_number,
             client=invoice.client,
             settlement_date=None,
+            user=invoice.user,
         )
 
         for item in invoice.items.all():
@@ -52,17 +51,27 @@ def create_invoices_for_recurring():
             item.invoice = new_invoice
             item.save()
 
-        subject = _("New recurring invoice %(nr)s") % {"nr": invoice.invoice_number}
+        subject = _("New recurring invoice %(nr)s") % {"nr": new_invoice.invoice_number}
         content = _(
             "A new recurring invoice has been created\n"
             "Best regards,\n"
             "Invoice Manager"
         )
 
-        html = invoice.get_html_for_pdf()
+        html = new_invoice.get_html_for_pdf()
 
+        # create and open temporary file as invoice_file
         with tempfile.NamedTemporaryFile(suffix=".pdf") as invoice_file:
+            # write rendered PDF to invoice_file
             pisa.CreatePDF(html, dest=invoice_file)
 
-            files = [invoice_file.name]
-            invoice.user.send_email(subject, content, files)
+            # go to beginning of file
+            invoice_file.seek(0)
+
+            files = [
+                {
+                    "name": f"{new_invoice.invoice_number.replace('/', '-')}.pdf",
+                    "content": invoice_file.read(),
+                }
+            ]
+            new_invoice.user.send_email(subject, content, files)
