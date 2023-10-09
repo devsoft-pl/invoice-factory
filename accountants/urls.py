0811193d4462f34f0431129bceb1190c84ef8265
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-10-09 23:24:58 +0200

    dodanie urls do widokow

--- accountants/urls.py
+++ accountants/urls.py
@@ -1,4 +1,18 @@
 from django.urls import path
 
+from accountants.views import (create_accountant_view, delete_accountant_view,
+                               list_accountants_view, replace_accountant_view)
+
 app_name = "accountants"
-urlpatterns = []
+urlpatterns = [
+    path("", list_accountants_view, name="list_accountants"),
+    path("create/", create_accountant_view, name="create_accountant"),
+    path(
+        "replace/<int:accountant_id>/",
+        replace_accountant_view,
+        name="replace_accountant",
+    ),
+    path(
+        "delete/<int:accountants_id>/", delete_accountant_view, name="delete_accountant"
+    ),
+]
