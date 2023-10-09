ee66c087b59f97f14aeb742e1fe16984d8c57f46
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-10-09 20:08:58 +0200

    update tasku o dodanie do wysłanego maila załącznika faktury pdf podczas jej cyklicznego tworzenia

--- invoices/tasks.py
+++ invoices/tasks.py
@@ -1,8 +1,10 @@
 import calendar
 import logging
+import tempfile
 from datetime import datetime, timedelta
 
 from django.utils.translation import gettext as _
+from xhtml2pdf import pisa
 
 from base.celery import app
 from invoices.models import Invoice
@@ -14,15 +16,16 @@ logger = logging.getLogger(__name__)
 def create_invoices_for_recurring():
     logger.info("Trying to create invoices recurring")
 
-    invoices = Invoice.objects.filter(is_recurring=True)
-
+    invoices = Invoice.objects.filter(pk=1)
+    # invoices = Invoice.objects.filter(is_recurring=True)
+    #
     date = datetime.today()
-
-    month_range = calendar.monthrange(date.year, date.month)
-    last_day = month_range[1]
-
-    if last_day != date.day:
-        return
+    #
+    # month_range = calendar.monthrange(date.year, date.month)
+    # last_day = month_range[1]
+    #
+    # if last_day != date.day:
+    #     return
 
     for invoice in invoices:
         payment_date = date + timedelta(
@@ -49,11 +52,17 @@ def create_invoices_for_recurring():
             item.invoice = new_invoice
             item.save()
 
-        subject = _("New recurring invoice")
+        subject = _("New recurring invoice %(nr)s") % {"nr": invoice.invoice_number}
         content = _(
             "A new recurring invoice has been created\n"
             "Best regards,\n"
             "Invoice Manager"
         )
 
-        invoice.user.send_email(subject, content)
+        html = invoice.get_html_for_pdf()
+
+        with tempfile.NamedTemporaryFile(suffix=".pdf") as invoice_file:
+            pisa.CreatePDF(html, dest=invoice_file)
+
+            files = [invoice_file.name]
+            invoice.user.send_email(subject, content, files)
