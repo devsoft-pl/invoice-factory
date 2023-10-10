e7e6544ff54f4aaffa18d2147e10062524c6cbf7
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-10-10 17:46:44 +0200

    zmiana nazwy wisoku głownej listy z ksiegowscia

--- accountants/urls.py
+++ accountants/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
 from accountants.views import (create_accountant_view, delete_accountant_view,
-                               detail_accountant_view, replace_accountant_view)
+                               list_accountants_view, replace_accountant_view)
 
 app_name = "accountants"
 urlpatterns = [
-    path("accountant/", detail_accountant_view, name="detail_accountant"),
+    path("", list_accountants_view, name="list_accountants"),
     path("create/", create_accountant_view, name="create_accountant"),
     path("replace/", replace_accountant_view, name="replace_accountant"),
     path("delete/", delete_accountant_view, name="delete_accountant"),
