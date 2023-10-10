36227e49bd24b2a75d71760fbda1d45c2bd9e8c6
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-10-10 23:26:45 +0200

    dodanie do nowego pola numer telefonu + dodanie walidacji numery telefonu

--- accountants/forms.py
+++ accountants/forms.py
@@ -1,12 +1,19 @@
 from django import forms
+from django.core.validators import RegexValidator
+from django.utils.translation import gettext as _
+
 
 from accountants.models import Accountant
+
+phone_number_validator = RegexValidator(
+    r"^[0-9]{9,}$", _("Enter phone number with 9 numbers only")
+)
 
 
 class AccountantForm(forms.ModelForm):
     class Meta:
         model = Accountant
-        fields = ["name", "email"]
+        fields = ["name", "email", "phone_number"]
 
     def __init__(self, user, *args, **kwargs):
         self.user = user
@@ -14,3 +21,5 @@ class AccountantForm(forms.ModelForm):
 
         for field in self.Meta.fields:
             self.fields[field].widget.attrs["class"] = "form-control"
+
+        self.fields["phone_number"].validators = [phone_number_validator]
