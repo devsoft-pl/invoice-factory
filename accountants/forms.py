2c7ebfabdb381b82ae16d7d45ab426a52f856782
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-10-10 23:06:24 +0200

    dodanie form-control fla pol z ksiegowosci

--- accountants/forms.py
+++ accountants/forms.py
@@ -4,10 +4,13 @@ from accountants.models import Accountant
 
 
 class AccountantForm(forms.ModelForm):
+    class Meta:
+        model = Accountant
+        fields = ["name", "email"]
+
     def __init__(self, user, *args, **kwargs):
         self.user = user
         super().__init__(*args, **kwargs)
 
-    class Meta:
-        model = Accountant
-        fields = ["name", "email"]
+        for field in self.Meta.fields:
+            self.fields[field].widget.attrs["class"] = "form-control"
