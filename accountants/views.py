cb8c9258d82a4dca3a5cb88dcbbf95521db0f7db
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-10-10 17:56:56 +0200

    dodanie widoku do wyswietlenia strony głównej z ksiegowoscia

--- accountants/views.py
+++ accountants/views.py
@@ -1,3 +1,27 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-# Create your views here.
+from accountants.models import Accountant
+
+
+@login_required
+def list_accountants_view(request):
+    accountants = Accountant.objects.filter(user=request.user)
+
+    context = {"accountants": accountants, "current_module": "accountants"}
+    return render(request, "accountants/list_accountants.html", context)
+
+
+@login_required
+def create_accountant_view(request):
+    pass
+
+
+@login_required
+def replace_accountant_view(request):
+    pass
+
+
+@login_required
+def delete_accountant_view(request):
+    pass
