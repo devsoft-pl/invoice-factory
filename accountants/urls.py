1d4afac53adbb957561d024a96820916fb65ee0d
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-10-10 17:43:10 +0200

    dodanie urli do nowych widoków z ksiegowoscia

--- accountants/urls.py
+++ accountants/urls.py
@@ -1,18 +1,12 @@
 from django.urls import path
 
 from accountants.views import (create_accountant_view, delete_accountant_view,
-                               list_accountants_view, replace_accountant_view)
+                               detail_accountant_view, replace_accountant_view)
 
 app_name = "accountants"
 urlpatterns = [
-    path("", list_accountants_view, name="list_accountants"),
+    path("accountant/", detail_accountant_view, name="detail_accountant"),
     path("create/", create_accountant_view, name="create_accountant"),
-    path(
-        "replace/<int:accountant_id>/",
-        replace_accountant_view,
-        name="replace_accountant",
-    ),
-    path(
-        "delete/<int:accountants_id>/", delete_accountant_view, name="delete_accountant"
-    ),
+    path("replace/", replace_accountant_view, name="replace_accountant"),
+    path("delete/", delete_accountant_view, name="delete_accountant"),
 ]
