a2cdc05cc70b33cd4bfbd5c8adf5f4d41f6e6c74
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-10-12 00:35:06 +0200

    dodanie nowego urla do tworzenia odbiorce podsumowania miesiąca

--- companies/urls.py
+++ companies/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from companies.views import (create_company_view, delete_company_view,
-                             detail_company_view, list_companies_view,
-                             replace_company_view, settings_company_view)
+from companies.views import (create_company_view,
+                             create_month_summary_recipient_view,
+                             delete_company_view, detail_company_view,
+                             list_companies_view, replace_company_view,
+                             settings_company_view)
 
 app_name = "companies"
 urlpatterns = [
@@ -24,4 +26,9 @@ urlpatterns = [
     path("replace/<int:company_id>/", replace_company_view, name="replace_company"),
     path("delete/<int:company_id>/", delete_company_view, name="delete_company"),
     path("settings/<int:company_id>/", settings_company_view, name="settings_company"),
+    path(
+        "settings/<int:company_id>/month_summary_recipient/create/",
+        create_month_summary_recipient_view,
+        name="create_month_summary_recipient",
+    ),
 ]
