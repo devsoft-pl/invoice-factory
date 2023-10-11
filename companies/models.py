41ac240cf29803adb92b3b4f35a0b867054274c9
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-10-11 22:56:56 +0200

    dodanie nowego modelu odbiorce podsumowania miesiaca do apliakcji companies

--- companies/models.py
+++ companies/models.py
@@ -41,3 +41,29 @@ class Company(models.Model):
         verbose_name_plural = _("companies")
         ordering = ["name"]
         unique_together = ["nip", "regon", "user"]
+
+
+class MonthSummaryRecipient(models.Model):
+    MONTHLY = 0
+    QUARTERLY = 1
+
+    SETTLEMENT_TYPES = (
+        (MONTHLY, _("Monthly")),
+        (QUARTERLY, _("Quarterly")),
+    )
+
+    description = models.CharField(verbose_name=_("Description"), max_length=50)
+    company = models.ForeignKey(
+        Company,
+        verbose_name=_("Company"),
+        on_delete=models.CASCADE,
+        related_name="month_summary_recipient",
+    )
+    day = models.IntegerField(verbose_name=_("Day of send"))
+    email = models.EmailField(_("Email"))
+    settlement_types = models.IntegerField(
+        verbose_name=_("settlement_types"), choices=SETTLEMENT_TYPES
+    )
+
+    def __str__(self):
+        return self.description
