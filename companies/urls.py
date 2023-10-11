8a8f8b38819eed11d41c90781e69fadcd8c2aa4a
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-10-11 23:23:20 +0200

    dodanie do urli szczegolow ustawien firmy

--- companies/urls.py
+++ companies/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from companies.views import (create_company_view, delete_company_view,
                              detail_company_view, list_companies_view,
-                             replace_company_view)
+                             replace_company_view, settings_company_view)
 
 app_name = "companies"
 urlpatterns = [
@@ -23,4 +23,5 @@ urlpatterns = [
     ),
     path("replace/<int:company_id>/", replace_company_view, name="replace_company"),
     path("delete/<int:company_id>/", delete_company_view, name="delete_company"),
+    path("settings/<int:company_id>/", settings_company_view, name="settings_company"),
 ]
