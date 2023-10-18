2b1c3bd2759638547e6544890bb5addb279face6
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-10-18 22:28:10 +0200

    dodanie nowego urla do edycji odbiorcy podsumowania

--- companies/urls.py
+++ companies/urls.py
@@ -4,7 +4,8 @@ from companies.views import (create_company_view,
                              create_summary_recipient_view,
                              delete_company_view, detail_company_view,
                              list_companies_view, replace_company_view,
-                             list_summary_recipients_view, delete_summary_recipient_view)
+                             list_summary_recipients_view, delete_summary_recipient_view,
+                             replace_summary_recipient_view)
 
 app_name = "companies"
 urlpatterns = [
@@ -29,4 +30,5 @@ urlpatterns = [
     path("<int:company_id>/summary_recipients/", list_summary_recipients_view, name="list_summary_recipients"),
     path("<int:company_id>/summary_recipient/create/", create_summary_recipient_view, name="create_summary_recipient"),
     path("summary_recipient/delete/<int:summary_recipient_id>/", delete_summary_recipient_view, name="delete_summary_recipient"),
+    path("summary_recipient/replace/<int:summary_recipient_id>/", replace_summary_recipient_view, name="replace_summary_recipient"),
 ]
