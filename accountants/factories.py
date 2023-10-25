6ddf525364d8cdac3ee98ad3c58417a281b9c5b1
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-10-25 14:20:35 +0200

    dodanie faktorki AccountantFactory

---
+++ accountants/factories.py
@@ -0,0 +1,14 @@
+import factory
+
+from accountants.models import Accountant
+from users.factories import UserFactory
+
+
+class AccountantFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Accountant
+
+    name = factory.Sequence(lambda n: "Name %03d" % n)
+    email = factory.Sequence(lambda n: "test_%03d@test.pl" % n)
+    phone_number = factory.Sequence(lambda n: "Phone %03d" % n)
+    user = factory.SubFactory(UserFactory)
