da2a9fa01e3f26129ddac3a8639573a3852fd2a7
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-10-25 08:42:50 +0200

    dodanie SummaryRecipientDictFactory

--- companies/factories.py
+++ companies/factories.py
@@ -36,12 +36,18 @@ class CompanyDictFactory(factory.DictFactory):
 
 
 class SummaryRecipientFactory(factory.django.DjangoModelFactory):
-
     class Meta:
         model = SummaryRecipient
 
     description = factory.Sequence(lambda n: "Description %03d" % n)
     company = factory.SubFactory(CompanyFactory)
+    day = fuzzy.FuzzyInteger(1, 28)
+    email = factory.Sequence(lambda n: "test_%03d@test.pl" % n)
+    settlement_types = SummaryRecipient.MONTHLY
+
+
+class SummaryRecipientDictFactory(factory.DictFactory):
+    description = factory.Sequence(lambda n: "Description %03d" % n)
     day = fuzzy.FuzzyInteger(1, 28)
     email = factory.Sequence(lambda n: "test_%03d@test.pl" % n)
     settlement_types = SummaryRecipient.MONTHLY
