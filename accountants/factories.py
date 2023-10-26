d329c1b6cbffdb61609da7d179a17772e98e6089
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-10-26 11:28:20 +0200

    dodanie dicta dla ksiegowosci w faktorce

--- accountants/factories.py
+++ accountants/factories.py
@@ -12,3 +12,9 @@ class AccountantFactory(factory.django.DjangoModelFactory):
     email = factory.Sequence(lambda n: "test_%03d@test.pl" % n)
     phone_number = factory.Sequence(lambda n: "Phone %03d" % n)
     user = factory.SubFactory(UserFactory)
+
+
+class AccountantDictFactory(factory.DictFactory):
+    name = factory.Sequence(lambda n: "Name %03d" % n)
+    email = factory.Sequence(lambda n: "test_%03d@test.pl" % n)
+    phone_number = factory.Sequence(lambda n: "Phone %03d" % n)
