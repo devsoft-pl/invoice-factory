7288e7b986bccb34db84ac74037655ab955f7ff7
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-11-01 16:48:55 +0100

    refaktor

--- accountants/tests/test_forms.py
+++ accountants/tests/test_forms.py
@@ -10,17 +10,18 @@ class TestAccountantForm:
     @pytest.fixture(autouse=True)
     def set_up(self) -> None:
         self.user = UserFactory.create()
-        self.accountant = AccountantFactory.create(user=self.user)
 
     def test_form_with_valid_data(self):
-        data = AccountantDictFactory(phone_number="123456789", email="test@test.pl")
+        data = AccountantDictFactory(phone_number="123456789")
+
         form = AccountantForm(data=data, user=self.user)
 
         assert form.is_valid()
         assert form.errors == {}
 
     def test_form_with_not_valid_data(self):
-        data = AccountantDictFactory(email="test@test.pl")
+        data = AccountantDictFactory()
+
         form = AccountantForm(data=data, user=self.user)
 
         assert not form.is_valid()
