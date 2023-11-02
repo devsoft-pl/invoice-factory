b9546d8fbe56906ed33fa1a22cdd3a18abe0ce19
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-11-03 00:48:18 +0100

    dodanie nowego url dla widoku SummaryRecipient

--- base/urls_api.py
+++ base/urls_api.py
@@ -7,6 +7,7 @@ from countries.views_api import CountryViewSet
 from currencies.views_api import CurrencyViewSet
 from invoices.views_api import InvoiceViewSet
 from items.views_api import ItemViewSet
+from summary_recipients.views_api import SummaryRecipientViewSet
 from users.views_api import UserViewSet
 from vat_rates.views_api import VatRateViewSet
 
@@ -19,6 +20,7 @@ router.register(r"companies", CompanyViewSet)
 router.register(r"invoices", InvoiceViewSet)
 router.register(r"items", ItemViewSet)
 router.register(r"accountants", AccountantViewSet)
+router.register(r"summary_recipients", SummaryRecipientViewSet)
 
 
 urlpatterns = [
