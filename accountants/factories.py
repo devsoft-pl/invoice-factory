d0827e47cb223b3526079b51cf49414878d61f6f
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-11-02 23:23:44 +0100

    dodanie nowego pola company i usuniecie pola user

--- accountants/factories.py
+++ accountants/factories.py
@@ -1,7 +1,7 @@
 import factory
 
 from accountants.models import Accountant
-from users.factories import UserFactory
+from companies.factories import CompanyFactory
 
 
 class AccountantFactory(factory.django.DjangoModelFactory):
@@ -11,7 +11,7 @@ class AccountantFactory(factory.django.DjangoModelFactory):
     name = factory.Sequence(lambda n: "Name %03d" % n)
     email = factory.Sequence(lambda n: "test_%03d@test.pl" % n)
     phone_number = factory.Sequence(lambda n: "Phone %03d" % n)
-    user = factory.SubFactory(UserFactory)
+    company = factory.SubFactory(CompanyFactory)
 
 
 class AccountantDictFactory(factory.DictFactory):
