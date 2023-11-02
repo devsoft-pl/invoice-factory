3906b07bbf8f615afc97edfc19d7157893086600
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-11-02 23:21:24 +0100

    dodanie nowego pola company do modelu i usuniecie pola user

--- accountants/models.py
+++ accountants/models.py
@@ -1,17 +1,21 @@
 from django.db import models
 from django.utils.translation import gettext as _
 
+from companies.models import Company
 from users.models import User
 
 
 class Accountant(models.Model):
     name = models.CharField(_("Name"), max_length=100, default=None)
-    email = models.EmailField(_("Email"), unique=True)
+    email = models.EmailField(_("Email"))
     phone_number = models.CharField(
         verbose_name=_("Phone number"), max_length=20, null=True, blank=True
     )
-    user = models.ForeignKey(
-        User, verbose_name=_("User"), on_delete=models.CASCADE, null=True
+    company = models.ForeignKey(
+        Company,
+        verbose_name=_("Company"),
+        on_delete=models.CASCADE,
+        related_name="summary_recipient",
     )
 
     def __str__(self):
@@ -19,3 +23,4 @@ class Accountant(models.Model):
 
     class Meta:
         verbose_name_plural = _("accountants")
+        unique_together = ["email", "company"]
