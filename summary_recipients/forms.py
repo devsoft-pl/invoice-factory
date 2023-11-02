872778a98fc342829fbeb8b530932bf6011c4fcf
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-11-03 00:36:14 +0100

    utworzenie formularza  dla odbiorcy podsumowania

---
+++ summary_recipients/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+
+from summary_recipients.models import SummaryRecipient
+
+
+class SummaryRecipientForm(forms.ModelForm):
+    class Meta:
+        model = SummaryRecipient
+        fields = [
+            "description",
+            "day",
+            "email",
+            "settlement_types",
+        ]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for field in self.Meta.fields:
+            self.fields[field].widget.attrs["class"] = "form-control"
