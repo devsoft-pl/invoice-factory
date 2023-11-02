cda4e850058f374d18f4998126b670d17a535f1e
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-11-03 00:57:11 +0100

    przeniesienie modelu odbiorcy podsumowania do osobnej aplikacji

--- companies/models.py
+++ companies/models.py
@@ -41,29 +41,3 @@ class Company(models.Model):
         verbose_name_plural = _("companies")
         ordering = ["name"]
         unique_together = ["nip", "regon", "user"]
-
-
-class SummaryRecipient(models.Model):
-    MONTHLY = 0
-    QUARTERLY = 1
-
-    SETTLEMENT_TYPES = (
-        (MONTHLY, _("Monthly")),
-        (QUARTERLY, _("Quarterly")),
-    )
-
-    description = models.CharField(verbose_name=_("Description"), max_length=50)
-    company = models.ForeignKey(
-        Company,
-        verbose_name=_("Company"),
-        on_delete=models.CASCADE,
-        related_name="summary_recipient",
-    )
-    day = models.IntegerField(verbose_name=_("Day of send"))
-    email = models.EmailField(_("Email"))
-    settlement_types = models.IntegerField(
-        verbose_name=_("Settlement types"), choices=SETTLEMENT_TYPES
-    )
-
-    def __str__(self):
-        return self.description
