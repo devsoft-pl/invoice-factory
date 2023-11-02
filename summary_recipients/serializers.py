2e3822376f0ab15c865048bdf53624f23d79c9cc
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-11-03 00:44:32 +0100

    utworzenie serializera dla odbiorcy podsumowania

---
+++ summary_recipients/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+
+from summary_recipients.models import SummaryRecipient
+
+
+class SummaryRecipientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SummaryRecipient
+        fields = [
+            "id",
+            "description",
+            "company",
+            "day",
+            "email",
+            "settlement_types",
+        ]
