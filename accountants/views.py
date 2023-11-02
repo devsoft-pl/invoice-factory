5988f0963a0a4ed2677631059edbaeae780bf806
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-11-02 23:26:56 +0100

    update wszsytkich metod o przypisanie ksiegowosci pod firme

--- accountants/views.py
+++ accountants/views.py
@@ -5,32 +5,42 @@ from django.utils.translation import gettext as _
 
 from accountants.forms import AccountantForm
 from accountants.models import Accountant
+from companies.models import Company
 
 
 @login_required
-def list_accountants_view(request):
-    accountants = Accountant.objects.filter(user=request.user)
+def list_accountants_view(request, company_id):
+    company = get_object_or_404(Company, pk=company_id)
+    accountants = Accountant.objects.filter(company=company)
 
-    context = {"accountants": accountants, "current_module": "accountants"}
+    if company.user != request.user:
+        raise Http404(_("Company does not exist"))
+
+    context = {"company": company, "accountants": accountants}
     return render(request, "accountants/list_accountants.html", context)
 
 
 @login_required
-def create_accountant_view(request):
+def create_accountant_view(request, company_id):
+    company = get_object_or_404(Company, pk=company_id)
+
+    if company.user != request.user:
+        raise Http404(_("Company does not exist"))
+
     if request.method != "POST":
-        form = AccountantForm(user=request.user)
+        form = AccountantForm()
     else:
-        form = AccountantForm(data=request.POST, user=request.user)
+        form = AccountantForm(data=request.POST)
 
         if form.is_valid():
             accountant = form.save(commit=False)
-            accountant.user = request.user
+            accountant.company = company
 
             accountant.save()
 
-            return redirect("accountants:list_accountants")
+            return redirect("accountants:list_accountants", company.pk)
 
-    context = {"form": form}
+    context = {"form": form, "company": company}
     return render(request, "accountants/create_accountant.html", context)
 
 
@@ -38,20 +48,24 @@ def create_accountant_view(request):
 def replace_accountant_view(request, accountant_id):
     accountant = get_object_or_404(Accountant, pk=accountant_id)
 
-    if accountant.user != request.user:
+    if accountant.company.user != request.user:
         raise Http404(_("Accountant does not exist"))
 
     if request.method != "POST":
-        form = AccountantForm(instance=accountant, user=request.user)
+        form = AccountantForm(instance=accountant)
     else:
-        form = AccountantForm(instance=accountant, data=request.POST, user=request.user)
+        form = AccountantForm(instance=accountant, data=request.POST)
 
         if form.is_valid():
             form.save()
 
-            return redirect("accountants:list_accountants")
+            return redirect("accountants:list_accountants", accountant.company.pk)
 
-    context = {"accountant": accountant, "form": form}
+    context = {
+        "accountant": accountant,
+        "form": form,
+        "company": accountant.company,
+    }
     return render(request, "accountants/replace_accountant.html", context)
 
 
@@ -59,9 +73,9 @@ def replace_accountant_view(request, accountant_id):
 def delete_accountant_view(request, accountant_id):
     accountant = get_object_or_404(Accountant, pk=accountant_id)
 
-    if accountant.user != request.user:
+    if accountant.company.user != request.user:
         raise Http404(_("Accountant does not exist"))
 
     accountant.delete()
 
-    return redirect("accountants:list_accountants")
+    return redirect("accountants:list_accountants", accountant.company.pk)
