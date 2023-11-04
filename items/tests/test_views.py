59cfdce43a8e0bf17aea0aa21c5545647bc3a28d
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-11-04 14:40:18 +0100

    refaktor testow widoku dla items

--- items/tests/test_views.py
+++ items/tests/test_views.py
@@ -3,7 +3,7 @@ from django.test import TestCase
 from django.urls import reverse
 
 from invoices.factories import InvoiceSellFactory
-from items.factories import ItemFactory
+from items.factories import ItemFactory, ItemDictFactory
 from items.models import Item
 from users.factories import UserFactory
 from vat_rates.factories import VatRateFactory
@@ -14,39 +14,11 @@ class TestItem(TestCase):
         self.user = UserFactory()
         self.user.set_password("test")
         self.user.save()
-        self.user_items = ItemFactory.create_batch(
-            12, user=self.user, invoice__user=self.user
-        )
-        self.other_item = ItemFactory()
+
         self.invoice = InvoiceSellFactory(user=self.user)
         self.vat = VatRateFactory(user=self.user)
-
-
-class TestDeleteItem(TestItem):
-    def setUp(self) -> None:
-        super().setUp()
-        self.item = self.user_items[0]
-        self.url = reverse("items:delete_item", args=[self.item.pk])
-
-    def test_delete_if_not_logged(self):
-        response = self.client.get(self.url, follow=True)
-
-        self.assertRedirects(response, f"/users/login/?next={self.url}")
-
-    def test_delete_if_logged(self):
-        self.client.login(username=self.user.email, password="test")
-        response = self.client.get(self.url)
-
-        with self.assertRaises(ObjectDoesNotExist):
-            Item.objects.get(pk=self.item.pk)
-        self.assertEqual(response.status_code, 302)
-
-    def test_return_404_if_not_my_item(self):
-        url = reverse("items:delete_item", args=[self.other_item.pk])
-        self.client.login(username=self.user.email, password="test")
-        response = self.client.get(url)
-
-        self.assertEqual(response.status_code, 404)
+        self.user_items = ItemFactory.create_batch(12, invoice=self.invoice, vat=self.vat)
+        self.other_item = ItemFactory()
 
 
 class TestCreateItem(TestItem):
@@ -61,6 +33,7 @@ class TestCreateItem(TestItem):
 
     def test_invalid_form_display_errors(self):
         self.client.login(username=self.user.email, password="test")
+
         response = self.client.post(self.url, {})
 
         self.assertEqual(response.status_code, 200)
@@ -73,16 +46,9 @@ class TestCreateItem(TestItem):
 
     def test_create_with_valid_data(self):
         self.client.login(username=self.user.email, password="test")
-        response = self.client.post(
-            self.url,
-            {
-                "name": "test",
-                "pkwiu": "62.01.1",
-                "amount": "1",
-                "net_price": "12000",
-                "vat": self.vat.pk,
-            },
-        )
+
+        item_date = ItemDictFactory(vat=self.vat.pk)
+        response = self.client.post(self.url, item_date)
 
         self.assertEqual(response.status_code, 302)
         self.assertRedirects(
@@ -90,28 +56,14 @@ class TestCreateItem(TestItem):
         )
         self.assertTrue(
             Item.objects.filter(
-                name="test",
-                pkwiu="62.01.1",
-                amount="1",
-                net_price="12000",
-                vat=self.vat.pk,
-                user=self.user,
+                name=item_date["name"],
+                pkwiu=item_date["pkwiu"]
             ).exists()
-        )
-        self.assertEqual(
-            Item.objects.filter(
-                name="test",
-                pkwiu="62.01.1",
-                amount="1",
-                net_price="12000",
-                vat=self.vat.pk,
-                user=self.user,
-            ).count(),
-            1,
         )
 
     def test_get_form(self):
         self.client.login(username=self.user.email, password="test")
+
         response = self.client.get(self.url)
 
         self.assertEqual(response.status_code, 200)
@@ -128,15 +80,9 @@ class TestReplaceItem(TestItem):
 
         self.assertRedirects(response, f"/users/login/?next={self.url}")
 
-    def test_return_404_if_not_my_item(self):
-        url = reverse("items:replace_item", args=[self.other_item.pk])
-        self.client.login(username=self.user.email, password="test")
-        response = self.client.get(url)
-
-        self.assertEqual(response.status_code, 404)
-
     def test_invalid_form_display_errors(self):
         self.client.login(username=self.user.email, password="test")
+
         response = self.client.post(self.url, {})
 
         self.assertEqual(response.status_code, 200)
@@ -152,44 +98,60 @@ class TestReplaceItem(TestItem):
     def test_replace_with_valid_data(self):
         self.client.login(username=self.user.email, password="test")
 
-        response = self.client.post(
-            self.url,
-            {
-                "name": "test2",
-                "pkwiu": "62.01.1",
-                "amount": "1",
-                "net_price": "12000",
-                "vat": self.vat.pk,
-            },
-        )
+        item_date = ItemDictFactory(vat=self.vat.pk)
+        response = self.client.post(self.url, item_date)
+
         self.assertEqual(response.status_code, 302)
         self.assertRedirects(
             response, reverse("invoices:detail_invoice", args=[self.item.invoice.pk])
         )
         self.assertTrue(
             Item.objects.filter(
-                name="test2",
-                pkwiu="62.01.1",
-                amount="1",
-                net_price="12000",
-                vat=self.vat.pk,
-                user=self.user,
+                name=item_date["name"],
+                pkwiu=item_date["pkwiu"]
             ).exists()
         )
-        self.assertEqual(
-            Item.objects.filter(
-                name="test2",
-                pkwiu="62.01.1",
-                amount="1",
-                net_price="12000",
-                vat=self.vat.pk,
-                user=self.user,
-            ).count(),
-            1,
-        )
+
+    def test_return_404_if_not_my_item(self):
+        self.client.login(username=self.user.email, password="test")
+
+        url = reverse("items:replace_item", args=[self.other_item.pk])
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 404)
 
     def test_get_form(self):
         self.client.login(username=self.user.email, password="test")
+
         response = self.client.get(self.url)
 
         self.assertEqual(response.status_code, 200)
+
+
+class TestDeleteItem(TestItem):
+    def setUp(self) -> None:
+        super().setUp()
+        self.item = self.user_items[0]
+        self.url = reverse("items:delete_item", args=[self.item.pk])
+
+    def test_delete_if_not_logged(self):
+        response = self.client.get(self.url, follow=True)
+
+        self.assertRedirects(response, f"/users/login/?next={self.url}")
+
+    def test_delete_if_logged(self):
+        self.client.login(username=self.user.email, password="test")
+
+        response = self.client.get(self.url)
+
+        with self.assertRaises(ObjectDoesNotExist):
+            Item.objects.get(pk=self.item.pk)
+        self.assertEqual(response.status_code, 302)
+
+    def test_return_404_if_not_my_item(self):
+        self.client.login(username=self.user.email, password="test")
+
+        url = reverse("items:delete_item", args=[self.other_item.pk])
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 404)
