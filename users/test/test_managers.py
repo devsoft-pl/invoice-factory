e6ee3dc50b179c2f0f921c9032d60a321e22aa7c
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-11-08 22:27:34 +0100

    refaktor

--- users/test/test_managers.py
+++ users/test/test_managers.py
@@ -1,4 +1,4 @@
-from unittest.mock import Mock, patch
+from unittest.mock import patch
 
 import pytest
 
@@ -8,21 +8,22 @@ from users.managers import UserManager
 
 @pytest.mark.django_db
 class TestUserManager:
+    @pytest.fixture(autouse=True)
+    def set_up(self) -> None:
+        self.user = UserFactory.create()
+
     @patch("users.managers.UserManager._create_user")
     def test_returns_created_user(self, _create_user_mock):
-        self.user = UserFactory()
         _create_user_mock.return_value = self.user
 
         assert UserManager().create_user(email=self.user.email) == self.user
 
     @patch("users.managers.UserManager._create_user")
     def test_returns_created_superuser(self, _create_user_mock):
-        self.superuser = UserFactory(is_superuser=True, is_staff=True)
-        _create_user_mock.return_value = self.superuser
+        superuser = UserFactory(is_superuser=True, is_staff=True)
+        _create_user_mock.return_value = superuser
 
-        assert (
-            UserManager().create_superuser(email=self.superuser.email) == self.superuser
-        )
+        assert UserManager().create_superuser(email=superuser.email) == superuser
 
     @pytest.mark.parametrize(
         "is_staff, is_superuser", [[False, False], [True, False], [False, True]]
@@ -30,9 +31,7 @@ class TestUserManager:
     def test_returns_raise_when_create_superuser_if_staff_and_superuser_is_false(
         self, is_staff, is_superuser
     ):
-        user = UserFactory()
-
         with pytest.raises(ValueError):
             UserManager().create_superuser(
-                email=user.email, is_staff=is_staff, is_superuser=is_superuser
+                email=self.user.email, is_staff=is_staff, is_superuser=is_superuser
             )
