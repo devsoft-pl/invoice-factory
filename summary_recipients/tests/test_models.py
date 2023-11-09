c754096a495fbe9e5752f05520412a1b1a183001
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-11-09 13:06:17 +0100

    testy dla modelu sprawdzajacego wysyłanie email z podsumowaniem do odbiorcy

--- summary_recipients/tests/test_models.py
+++ summary_recipients/tests/test_models.py
@@ -1,3 +1,5 @@
+from unittest.mock import patch
+
 import pytest
 
 from summary_recipients.factories import SummaryRecipientFactory
@@ -11,3 +13,20 @@ class TestSummaryRecipientModel:
 
     def test_returns_str_description(self):
         assert self.summary_recipient.__str__() == self.summary_recipient.description
+
+    @patch("summary_recipients.models.EmailMessage.send")
+    def test_returns_sent_email_without_attachment(self, email_message_send_mock):
+        subject = "Test temat"
+        content = "Test zawartość"
+        self.summary_recipient.send_email(subject, content)
+
+        email_message_send_mock.assert_called_once()
+
+    @patch("summary_recipients.models.EmailMessage.attach")
+    def test_returns_sent_email_with_attachment(self, email_message_attach_mock):
+        subject = "Test temat"
+        content = "Test zawartość"
+        files = [{"name": "test.pdf", "content": "test"}]
+        self.summary_recipient.send_email(subject, content, files)
+
+        email_message_attach_mock.assert_called_once()
