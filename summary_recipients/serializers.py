1ea556b3c0a372e37bcbfcd7153cc8c7deac07a8
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-11-09 23:29:20 +0100

    black

--- summary_recipients/serializers.py
+++ summary_recipients/serializers.py
@@ -13,5 +13,5 @@ class SummaryRecipientSerializer(serializers.ModelSerializer):
             "day",
             "email",
             "settlement_types",
-            "final_call"
+            "final_call",
         ]
