5af1b89428fc4419882fef33050dc660867d2ea7
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-11-16 00:23:53 +0100

    test dla sprawdzenia rozliczenia faktury gdzie wysłane podsumowanie dla odbiorcy jest ostatecznym podsumowaniem

--- invoices/tests/test_tasks.py
+++ invoices/tests/test_tasks.py
@@ -49,7 +49,7 @@ class TestSummaryRecipientTasks:
         self.last_month_date_2 = self.last_month_date.replace(day=7)
         self.company = CompanyFactory.create(is_my_company=True)
         self.invoice_1 = InvoiceSellFactory.create(
-            company=self.company, create_date=self.last_month_date
+            company=self.company, create_date=self.last_month_date, is_settled=False
         )
         self.invoice_2 = InvoiceSellFactory.create(
             company=self.company, create_date=self.last_month_date_2
@@ -57,16 +57,6 @@ class TestSummaryRecipientTasks:
 
     @patch("summary_recipients.models.SummaryRecipient.send_email")
     def test_send_monthly_summary_in_defined_day(self, send_email_mock):
-        SummaryRecipientFactory.create(company=self.company, day=self.today.day)
-
-        send_monthly_summary_to_recipients()
-
-        send_email_mock.assert_called()
-        files = send_email_mock.call_args[0][2]
-        assert len(files) == 2
-
-    @patch("summary_recipients.models.SummaryRecipient.send_email")
-    def test_not_send_monthly_summary_if_not_defined_day(self, send_email_mock):
         SummaryRecipientFactory.create(company=self.company, day=self.today.day)
 
         send_monthly_summary_to_recipients()
@@ -84,3 +74,16 @@ class TestSummaryRecipientTasks:
         send_monthly_summary_to_recipients()
 
         send_email_mock.assert_not_called()
+
+    @pytest.mark.parametrize(
+        "final_call, expected_value", [[True, True], [False, False]]
+    )
+    def test_if_invoice_is_settled_on_final_call(self, final_call, expected_value):
+        SummaryRecipientFactory.create(
+            company=self.company, day=self.today.day, final_call=final_call
+        )
+
+        send_monthly_summary_to_recipients()
+        self.invoice_1.refresh_from_db()
+
+        assert self.invoice_1.is_settled is expected_value
