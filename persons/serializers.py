ec5a6f7e1db1c1e1c7854d2d67a20bf905bfb9d1
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-11-20 23:20:58 +0100

    feat: dodanie serializera do modelu person

---
+++ persons/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+
+from persons.models import Person
+
+
+class PersonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Person
+        fields = [
+            "id",
+            "first_name",
+            "last_name",
+            "address",
+            "zip_code",
+            "city",
+            "country",
+            "email",
+            "phone_number"
+        ]
