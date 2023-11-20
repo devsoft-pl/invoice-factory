b3499663c1ec9e182dad49fa6dfe57325ba48b13
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-11-20 23:08:54 +0100

    feat: dodanie nowego modelu person do aplikacji persons

--- persons/models.py
+++ persons/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils.translation import gettext as _
 
-# Create your models here.
+from countries.models import Country
+
+
+class Person(models.Model):
+    first_name = models.CharField(_("First name"), max_length=75)
+    last_name = models.CharField(_("Last name"), max_length=75)
+    address = models.CharField(verbose_name=_("Address"), max_length=100)
+    zip_code = models.CharField(verbose_name=_("ZIP Code"), max_length=10)
+    city = models.CharField(verbose_name=_("City"), max_length=60)
+    country = models.ForeignKey(
+        Country, verbose_name=_("Country"), on_delete=models.CASCADE, null=True
+    )
+    email = models.EmailField(verbose_name=_("Email"))
+    phone_number = models.CharField(
+        verbose_name=_("Phone number"), max_length=20)
