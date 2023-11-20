4b01c37893c00bea8d34460cca4f458851609c74
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-11-20 23:16:17 +0100

    feat: dodanie do admin nowego modelu person

--- persons/admin.py
+++ persons/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
-# Register your models here.
+from persons.models import Person
+
+
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "address", "zip_code", "city", "country")
+    search_fields = ("first_name", "last_name")
+
+    fieldsets = (
+        (
+            _("Person info"),
+            {
+                "fields": ("first_name", "last_name"),
+            },
+        ),
+        (
+            _("Basic information"),
+            {
+                "fields": ("address", "zip_code", "city", "country"),
+            },
+        ),
+        (
+            _("Additional information"),
+            {
+                "fields": ("email", "phone_number"),
+            },
+        ),
+    )
