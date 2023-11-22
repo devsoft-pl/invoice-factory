4f0d55fccba311a5f08daee8bee2bb986f25332c
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-11-22 18:40:57 +0100

    feat: dodanie metod restowych do widoku

--- persons/views.py
+++ persons/views.py
@@ -1,3 +1,87 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.http import Http404
+from django.shortcuts import get_object_or_404, redirect, render
+from django.utils.translation import gettext as _
 
-# Create your views here.
+from persons.forms import PersonForm
+from persons.models import Person
+
+
+@login_required
+def list_persons_view(request):
+    persons = Person.objects.filter(country__user=request.user)
+    paginator = Paginator(persons, 10)
+    page = request.GET.get("page")
+    try:
+        persons = paginator.page(page)
+    except PageNotAnInteger:
+        persons = paginator.page(1)
+    except EmptyPage:
+        persons = paginator.page(paginator.num_pages)
+
+    context = {"persons": persons, "current_module": "countries"}
+    return render(request, "persons/list_persons.html", context)
+
+
+@login_required
+def detail_person_view(request, person_id):
+    person = get_object_or_404(Person, pk=person_id)
+
+    if person.country.user != request.user:
+        raise Http404(_("Person does not exist"))
+
+    context = {"person": person}
+    return render(request, "persons/detail_person.html", context)
+
+
+@login_required
+def create_person_view(request):
+    if request.method != "POST":
+        form = PersonForm(country_user=request.user)
+    else:
+        form = PersonForm(country_user=request.user, data=request.POST)
+
+        if form.is_valid():
+            person = form.save(commit=False)
+            person.country.user = request.user
+
+            person.save()
+
+            return redirect("persons:list_persons")
+
+    context = {"form": form}
+    return render(request, "persons/create_person.html", context)
+
+
+@login_required
+def replace_person_view(request, person_id):
+    person = get_object_or_404(Person, pk=person_id)
+
+    if person.country.user != request.user:
+        raise Http404(_("Person does not exist"))
+
+    if request.method != "POST":
+        form = PersonForm(country_user=request.user, instance=person)
+    else:
+        form = PersonForm(country_user=request.user, instance=person, data=request.POST)
+
+    if form.is_valid():
+        form.save()
+
+        return redirect("persons:list_persons")
+
+    context = {"person": person, "form": form}
+    return render(request, "persons/replace_person.html", context)
+
+
+@login_required
+def delete_person_view(request, person_id):
+    person = get_object_or_404(Person, pk=person_id)
+
+    if person.country.user != request.user:
+        raise Http404(_("Person does not exist"))
+
+    person.delete()
+
+    return redirect("persons:list_persons")
