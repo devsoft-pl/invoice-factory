2cb20219b7f96baeda22a8e234a3bcf8f63905c2
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-11-22 22:25:53 +0100

    feat: add regex to fields in model + adding filter by fields

--- persons/forms.py
+++ persons/forms.py
@@ -5,6 +5,14 @@ from django.utils.translation import gettext as _
 from countries.models import Country
 from persons.models import Person
 
+first_name_validator = RegexValidator(
+    r"^[a-zA-Z ]+$", _("Enter the first_name in letters only")
+)
+
+last_name_validator = RegexValidator(
+    r"^[a-zA-Z ]+$", _("Enter the last_name in letters only")
+)
+
 zip_code_validator = RegexValidator(
     r"^[0-9]{2}-[0-9]{3}$", _("Zip code in numbers only in format xx-xxx")
 )
@@ -40,6 +48,32 @@ class PersonForm(forms.ModelForm):
         for field in self.Meta.fields:
             self.fields[field].widget.attrs["class"] = "form-control"
 
+        self.fields["first_name"].validators = [first_name_validator]
+        self.fields["last_name"].validators = [last_name_validator]
         self.fields["zip_code"].validators = [zip_code_validator]
         self.fields["city"].validators = [city_validator]
         self.fields["phone_number"].validators = [phone_number_validator]
+
+
+class PersonFilterForm(forms.Form):
+    first_name = forms.CharField(required=False)
+    last_name = forms.CharField(required=False)
+    address = forms.CharField(required=False)
+
+    first_name.widget.attrs.update({"class": "form-control"})
+    last_name.widget.attrs.update({"class": "form-control"})
+    address.widget.attrs.update({"class": "form-control"})
+
+    def get_filtered_persons(self, persons_list):
+        first_name = self.cleaned_data["first_name"]
+        last_name = self.cleaned_data["last_name"]
+        address = self.cleaned_data["address"]
+
+        if first_name:
+            persons_list = persons_list.filter(first_name__contains=first_name)
+        if last_name:
+            persons_list = persons_list.filter(last_name__contains=last_name)
+        if address:
+            persons_list = persons_list.filter(address__contains=address)
+
+        return persons_list
