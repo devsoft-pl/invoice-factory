84f24bc06754fe9fbeaef9faaa777f0ec4394f84
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2023-11-28 23:03:20 +0100

    feat: adding tests for detail person view

--- persons/tests/test_views.py
+++ persons/tests/test_views.py
@@ -12,8 +12,7 @@ class TestPerson(TestCase):
         self.user = UserFactory()
         self.user.set_password("test")
         self.user.save()
-        self.user_person = PersonFactory.create_batch(12, user=self.user)
-        self.other_person = PersonFactory()
+        self.user_persons = PersonFactory.create_batch(12, user=self.user)
 
 
 class TestListPersons(TestPerson):
@@ -60,3 +59,34 @@ class TestListPersons(TestPerson):
         object_list = response.context["persons"]
 
         self.assertTrue(len(object_list) == 2)
+
+
+class TestDetailPersons(TestPerson):
+    def setUp(self) -> None:
+        super().setUp()
+        self.person = self.user_persons[0]
+        self.url = reverse("persons:detail_person", args=[self.person.pk])
+
+    def test_detail_if_not_logged(self):
+        response = self.client.get(self.url, follow=True)
+
+        self.assertRedirects(response, f"/users/login/?next={self.url}")
+
+    def test_detail_if_logged(self):
+        self.client.login(username=self.user.email, password="test")
+
+        response = self.client.get(self.url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "persons/detail_person.html")
+        self.assertEqual(self.person.pk, response.context["person"].pk)
+
+    def test_return_404_if_not_my_person(self):
+        self.client.login(username=self.user.email, password="test")
+
+        self.other_person = PersonFactory()
+        url = reverse("persons:detail_person", args=[self.other_person.pk])
+
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 404)
