c0a69efab4fafc0f6085f1d1a7d872687ffecf69
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2024-01-17 21:29:17 +0100

    feat: add new path list_reports

--- reports/urls.py
+++ reports/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
 
+from reports.views import list_reports_view
+
 app_name = "reports"
-urlpatterns = []
+urlpatterns = [
+    path("", list_reports_view, name="list_reports"),
+]
