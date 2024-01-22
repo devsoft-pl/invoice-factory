e34f741afeff0174a279a093f99a942f0e3233f1
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2024-01-22 23:37:50 +0100

    feat: add filter to take invoice sales view

--- reports/views.py
+++ reports/views.py
@@ -7,12 +7,17 @@ from invoices.models import Invoice
 
 @login_required
 def list_reports_view(request):
-    invoices = list(Invoice.objects.annotate(month=ExtractMonth('sale_date')).values('month').annotate(sum_net=Sum('net_amount')))
+    net_invoices = list(Invoice.objects.filter(invoice_type=Invoice.INVOICE_SALES).annotate(month=ExtractMonth('sale_date')).values('month').annotate(sum_net=Sum('net_amount')))
 
-    net_sum_per_month = dict([str(invoice['month']), invoice['sum_net']] for invoice in invoices)
-    invoices = [{"month": month, "sum_net": net_sum_per_month.get(str(month), 0)} for month in range(1, 13)]
+    net_sum_per_month = dict([str(invoice['month']), invoice['sum_net']] for invoice in net_invoices)
+    net_invoices = [{"month": month, "sum_net": net_sum_per_month.get(str(month), 0)} for month in range(1, 13)]
 
-    context = {"invoices": invoices, "current_module": "reports"}
+    gross_invoices = list(Invoice.objects.filter(invoice_type=Invoice.INVOICE_SALES).annotate(month=ExtractMonth('sale_date')).values('month').annotate(gross_sum=Sum('gross_amount')))
+
+    gross_sum_per_month = dict([str(invoice['month']), invoice['gross_sum']] for invoice in gross_invoices)
+    gross_invoices = [{"month": month, "gross_sum": gross_sum_per_month.get(str(month), 0)} for month in range(1, 13)]
+
+    context = {"net_invoices": net_invoices, "gross_invoices": gross_invoices, "current_module": "reports"}
 
     return render(request, "reports/list_reports.html", context)
 
