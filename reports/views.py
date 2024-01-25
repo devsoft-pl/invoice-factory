f9e8af21b3051aabb4fac60abd2d1466626dce4d
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2024-01-25 22:39:39 +0100

    refactor

--- reports/views.py
+++ reports/views.py
@@ -1,84 +1,69 @@
 from django.contrib.auth.decorators import login_required
-from django.db.models import Sum
-from django.db.models.functions import ExtractMonth
 from django.shortcuts import render
 
 from invoices.models import Invoice
 from reports.forms import ReportFilterForm
 
 
-@login_required
-def list_reports_view(request):
-    net_invoices = list(
-        Invoice.objects.filter(invoice_type=Invoice.INVOICE_SALES)
-        .annotate(month=ExtractMonth("sale_date"))
-        .values("month")
-        .annotate(net_sum=Sum("net_amount"))
+def get_sum_invoices_per_month(invoices):
+    sum_per_month = dict(
+        [str(invoice["month"]), invoice["sum"]] for invoice in invoices
     )
-
-    net_sum_per_month = dict(
-        [str(invoice["month"]), invoice["net_sum"]] for invoice in net_invoices
-    )
-    net_invoices = [
-        {"month": month, "net_sum": net_sum_per_month.get(str(month), 0)}
+    invoices = [
+        {"month": month, "sum": sum_per_month.get(str(month), 0)}
         for month in range(1, 13)
     ]
 
+    return invoices
+
+
+@login_required
+def list_reports_view(request):
+    net_invoices = list(Invoice.objects.sales().with_months().with_sum("net_amount"))
+    net_invoices = get_sum_invoices_per_month(net_invoices)
+
+    net_sum_per_month = dict(
+        [str(invoice["month"]), invoice["sum"]] for invoice in net_invoices
+    )
     total_net_sum = sum(net_sum_per_month.values())
 
     gross_invoices = list(
-        Invoice.objects.filter(invoice_type=Invoice.INVOICE_SALES)
-        .annotate(month=ExtractMonth("sale_date"))
-        .values("month")
-        .annotate(gross_sum=Sum("gross_amount"))
+        Invoice.objects.sales().with_months().with_sum("gross_amount")
     )
+    gross_invoices = get_sum_invoices_per_month(gross_invoices)
 
     gross_sum_per_month = dict(
-        [str(invoice["month"]), invoice["gross_sum"]] for invoice in gross_invoices
+        [str(invoice["month"]), invoice["sum"]] for invoice in gross_invoices
     )
-    gross_invoices = [
-        {"month": month, "gross_sum": gross_sum_per_month.get(str(month), 0)}
-        for month in range(1, 13)
-    ]
-
     total_gross_sum = sum(gross_sum_per_month.values())
 
     filter_form = ReportFilterForm(request.GET)
+
+    extra_context = {
+        "net_invoices": net_invoices,
+        "total_net_sum": total_net_sum,
+        "gross_invoices": gross_invoices,
+        "total_gross_sum": total_gross_sum,
+        "invoices": zip(net_invoices, gross_invoices),
+    }
+
     if filter_form.is_valid():
         revenue_type = filter_form.cleaned_data["revenue_type"]
         if revenue_type == ReportFilterForm.NETTO:
-            context = {
+            extra_context = {
                 "net_invoices": net_invoices,
                 "total_net_sum": total_net_sum,
-                "filter_form": filter_form,
-                "current_module": "reports",
             }
         elif revenue_type == ReportFilterForm.GROSS:
-            context = {
+            extra_context = {
                 "gross_invoices": gross_invoices,
                 "total_gross_sum": total_gross_sum,
-                "filter_form": filter_form,
-                "current_module": "reports",
             }
-        else:
-            context = {
-                "net_invoices": net_invoices,
-                "total_net_sum": total_net_sum,
-                "gross_invoices": gross_invoices,
-                "total_gross_sum": total_gross_sum,
-                "invoices": zip(net_invoices, gross_invoices),
-                "filter_form": filter_form,
-                "current_module": "reports",
-            }
-    else:
-        context = {
-            "net_invoices": net_invoices,
-            "total_net_sum": total_net_sum,
-            "gross_invoices": gross_invoices,
-            "total_gross_sum": total_gross_sum,
-            "invoices": zip(net_invoices, gross_invoices),
-            "filter_form": filter_form,
-            "current_module": "reports",
-        }
-        breakpoint()
+
+    context = {
+        "filter_form": filter_form,
+        "current_module": "reports",
+    }
+    context.update(extra_context)
+
     return render(request, "reports/list_reports.html", context)
