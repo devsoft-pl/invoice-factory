ce674451e3a0297a401987ebcb396b7c561010d7
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2024-01-25 21:03:52 +0100

    feat: add total_net_sum and total_gross_sum for view

--- reports/views.py
+++ reports/views.py
@@ -24,6 +24,8 @@ def list_reports_view(request):
         for month in range(1, 13)
     ]
 
+    total_net_sum = sum(net_sum_per_month.values())
+
     gross_invoices = list(
         Invoice.objects.filter(invoice_type=Invoice.INVOICE_SALES)
         .annotate(month=ExtractMonth("sale_date"))
@@ -39,25 +41,31 @@ def list_reports_view(request):
         for month in range(1, 13)
     ]
 
+    total_gross_sum = sum(gross_sum_per_month.values())
+
     filter_form = ReportFilterForm(request.GET)
     if filter_form.is_valid():
         revenue_type = filter_form.cleaned_data["revenue_type"]
         if revenue_type == ReportFilterForm.NETTO:
             context = {
                 "net_invoices": net_invoices,
+                "total_net_sum": total_net_sum,
                 "filter_form": filter_form,
                 "current_module": "reports",
             }
         elif revenue_type == ReportFilterForm.GROSS:
             context = {
                 "gross_invoices": gross_invoices,
+                "total_gross_sum": total_gross_sum,
                 "filter_form": filter_form,
                 "current_module": "reports",
             }
         else:
             context = {
                 "net_invoices": net_invoices,
+                "total_net_sum": total_net_sum,
                 "gross_invoices": gross_invoices,
+                "total_gross_sum": total_gross_sum,
                 "invoices": zip(net_invoices, gross_invoices),
                 "filter_form": filter_form,
                 "current_module": "reports",
@@ -65,7 +73,9 @@ def list_reports_view(request):
     else:
         context = {
             "net_invoices": net_invoices,
+            "total_net_sum": total_net_sum,
             "gross_invoices": gross_invoices,
+            "total_gross_sum": total_gross_sum,
             "invoices": zip(net_invoices, gross_invoices),
             "filter_form": filter_form,
             "current_module": "reports",
