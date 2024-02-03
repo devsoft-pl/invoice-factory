3141bf5b7672f3ea1f2ee93658b2ec0f38e6efb2
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2024-02-03 18:23:11 +0100

    feat: add modal to create vat rate to create item in invoice

--- vat_rates/urls.py
+++ vat_rates/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 
-from vat_rates.views import (create_vat_view, delete_vat_view,
-                             list_vat_rates_view, replace_vat_view)
+from vat_rates.views import (create_vat_ajax_view, create_vat_view,
+                             delete_vat_view, list_vat_rates_view,
+                             replace_vat_view)
 
 app_name = "vat_rates"
 urlpatterns = [
     path("", list_vat_rates_view, name="list_vat_rates"),
     path("create/", create_vat_view, name="create_vat"),
+    path("create_ajax/", create_vat_ajax_view, name="create_vat_ajax"),
     path("replace/<int:vat_id>/", replace_vat_view, name="replace_vat"),
     path("delete/<int:vat_id>/", delete_vat_view, name="delete_vat"),
 ]
--- vat_rates/views.py
+++ vat_rates/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
-from django.http import Http404
+from django.http import Http404, JsonResponse
 from django.shortcuts import get_object_or_404, redirect, render
 from django.utils.translation import gettext as _
 
@@ -47,6 +47,34 @@ def create_vat_view(request):
 
     context = {"form": form}
     return render(request, "vat_rates/create_vat.html", context)
+
+
+@login_required
+def create_vat_ajax_view(request):
+    if request.method != "POST":
+        form = VatRateForm(user=request.user)
+    else:
+        form = VatRateForm(data=request.POST, user=request.user)
+
+        if form.is_valid():
+            vat_rate = form.save(commit=False)
+            vat_rate.user = request.user
+
+            vat_rate.save()
+
+            return JsonResponse(
+                {
+                    "success": True,
+                    "id": vat_rate.id,
+                    "name": vat_rate.rate,
+                }
+            )
+
+        else:
+            return JsonResponse({"success": False, "errors": form.errors})
+
+    context = {"form": form}
+    return render(request, "vat_rates/create_vat_ajax.html", context)
 
 
 @login_required
