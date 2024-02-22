ca0410d510a4315375ed7a7f9dd6c90d8358a53d
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2024-02-22 22:28:33 +0100

    feat: add autocomplete to off in field

--- invoices/forms.py
+++ invoices/forms.py
@@ -193,6 +193,11 @@ class InvoiceBuyForm(forms.ModelForm):
             self.fields[field].widget.attrs["class"] = "form-control"
             self.fields[field].required = True
 
+        self.fields["invoice_number"].widget.attrs["autocomplete"] = "off"
+        self.fields["sale_date"].widget.attrs["autocomplete"] = "off"
+        self.fields["payment_date"].widget.attrs["autocomplete"] = "off"
+        self.fields["settlement_date"].widget.attrs["autocomplete"] = "off"
+
     def clean_invoice_number(self):
         invoice_number = self.cleaned_data.get("invoice_number")
         invoice = Invoice.objects.filter(
