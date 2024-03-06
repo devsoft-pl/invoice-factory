939208f683db08bdfa65617d7b3a791035be8de7
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2024-03-06 23:28:06 +0100

    feat: add class d-none to is paid container element when is recurring checkbox is true

--- invoices/static/toggle_recurring_invoice_fields.js
+++ invoices/static/toggle_recurring_invoice_fields.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const isRecurringCheckbox = document.getElementById('id_is_recurring');
     const invoiceNumberInput = document.getElementById('id_invoice_number');
     const isLastDayContainerElement = document.getElementById('id_is_last_day').parentElement;
+    const isPaidContainerElement = document.getElementById('isPaidContainerElement');
 
 
     isRecurringCheckbox.addEventListener("change", (e) => {
@@ -9,10 +10,12 @@ document.addEventListener("DOMContentLoaded", function () {
 
         if (isRecurringCheckbox.checked) {
             invoiceNumberInput.disabled = true;
-            isLastDayContainerElement.classList.remove('d-none')
+            isLastDayContainerElement.classList.remove('d-none');
+            isPaidContainerElement.classList.add('d-none');
         } else {
             invoiceNumberInput.disabled = false;
-            isLastDayContainerElement.classList.add('d-none')
+            isLastDayContainerElement.classList.add('d-none');
+            isPaidContainerElement.classList.remove('d-none');
         }
     });
 });
