540325433406c4f3aef1512aac97362496b39921
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2024-04-03 22:29:24 +0200

    feat: update news errors to field

--- base/validators.py
+++ base/validators.py
@@ -3,31 +3,31 @@ from django.utils.translation import gettext as _
 
 invoice_number_validator = RegexValidator(
     r"^[0-9]+/(0[1-9]|1[0-2])/[0-9]{4}$",
-    _("Enter invoice number in numbers only in format number/mm/yyyy"),
+    _("Please enter the invoice number in numeric format only, following the pattern number/mm/yyyy"),
 )
 
 correction_invoice_number_validator = RegexValidator(
-    r"^[0-9]+/(0[1-9]|1[0-2])/[0-9]{4}/korekta$",
-    _("Enter correction invoice number in only in format number/mm/yyyy/correction"),
+    r"^[0-9]+/(0[1-9]|1[0-2])/[0-9]{4}/k$",
+    _("Please enter the correction invoice number in the format number/mm/yyyy/c"),
 )
 
 nip_validator = RegexValidator(
     r"^[0-9a-zA-Z]{8,16}$",
-    _("Enter the tax ID without special characters with minimum 8 character"),
+    _("Please enter the tax ID without special characters and with a minimum of 8 characters"),
 )
 
 regon_validator = RegexValidator(
     r"^([0-9]{9}|[0-9]{14})$",
-    _("Enter regon in numbers only with minimum 9 character"),
+    _("Please enter the REGON using numbers only, with a minimum of 9 characters"),
 )
 
 currency_validator = RegexValidator(
-    r"^[a-zA-Z]{3}$", _("Enter country three letter code")
+    r"^[a-zA-Z]{3}$", _("Please enter the country code using three letters")
 )
 
 account_number_validator = RegexValidator(
     r"^[0-9A-Z ]{15,32}$",
-    _("Enter account number with minimum 15 character without special characters"),
+    _("Please enter the account number with a minimum of 15 characters, excluding special characters"),
 )
 
 rate_validator = MaxValueValidator(99)
@@ -37,20 +37,20 @@ first_name_validator = RegexValidator(
 )
 
 last_name_validator = RegexValidator(
-    r"^[a-zA-Z ]+$", _("Enter the last_name in letters only")
+    r"^[a-zA-Z ]+$", _("Please enter the last name using letters only")
 )
 
 zip_code_validator = RegexValidator(
-    r"^[0-9]{2}-[0-9]{3}$", _("Zip code in numbers only in format xx-xxx")
+    r"^[0-9]{2}-[0-9]{3}$", _("Please enter the zip code using numbers only in the format xx-xxx")
 )
 
-city_validator = RegexValidator(r"^[a-zA-Z ]+$", _("Enter the city in letters only"))
+city_validator = RegexValidator(r"^[a-zA-Z ]+$", _("Please enter the city using letters only"))
 
 
 country_validator = RegexValidator(
-    r"^[a-zA-Z ]{2,}$", _("Enter the country in letters only")
+    r"^[a-zA-Z ]{2,}$", _("Please enter the country using letters only")
 )
 
 phone_number_validator = RegexValidator(
-    r"^[0-9]{9,}$", _("Enter phone number with 9 numbers only")
+    r"^[0-9]{9,}$", _("Please enter a phone number with 9 digits only")
 )
