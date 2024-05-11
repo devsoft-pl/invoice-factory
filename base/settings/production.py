3a31cc83f0133a9e61c25b65cc26f00516b5bfa9
Izabela Kaczynska <izabela@devsoft.pl>
2024-05-11 05:10:27 +0200

    fix: debug false on production

--- base/settings/production.py
+++ base/settings/production.py
@@ -6,3 +6,9 @@ DEFAULT_FILE_STORAGE = "base.storages.MediaStorage"
 STATICFILES_STORAGE = "base.storages.StaticStorage"
 
 DEBUG = False
+
+CSRF_TRUSTED_ORIGINS = [f"https://invoice-factory.devsoft.pl"]
+
+CORS_ALLOWED_ORIGINS = [
+    "https://invoice-factory.devsoft.pl",
+]
