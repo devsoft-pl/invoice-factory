0511bfe2b34d7bd8be1af2572500916aa75cbf98
Izabela Kaczynska <izabela@devsoft.pl>
2024-05-11 02:20:07 +0200

    fix: settings for production environment

--- base/settings/production.py
+++ base/settings/production.py
@@ -1,5 +1,6 @@
 from .common import *  # noqa: F403, F401 isort:skip
 
+ALLOWED_HOSTS = ["invoice-factory.devsoft.pl"]
 
 DEFAULT_FILE_STORAGE = "base.storages.MediaStorage"
 STATICFILES_STORAGE = "base.storages.StaticStorage"
