93986bcbae13de0239cfeaf1b68c30af08275cda
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2024-05-28 23:38:21 +0200

    feat: delete f-string

--- base/settings/production.py
+++ base/settings/production.py
@@ -7,7 +7,7 @@ STATICFILES_STORAGE = "base.storages.StaticStorage"
 
 DEBUG = False
 
-CSRF_TRUSTED_ORIGINS = [f"https://invoice-factory.devsoft.pl"]
+CSRF_TRUSTED_ORIGINS = ["https://invoice-factory.devsoft.pl"]
 
 CORS_ALLOWED_ORIGINS = [
     "https://invoice-factory.devsoft.pl",
