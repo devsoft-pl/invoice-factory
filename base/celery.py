d6531b2c480ac0286edd88cefe68691418b0d305
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2024-05-28 23:25:55 +0200

    fix: change django settings module for celery

--- base/celery.py
+++ base/celery.py
@@ -2,7 +2,7 @@ import os
 
 from celery import Celery
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "base.settings.dev")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "base.settings.production")
 
 app = Celery("invoice_manager")
 
