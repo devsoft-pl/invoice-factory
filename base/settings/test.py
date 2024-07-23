19fe9ed8695f50bc23fa72673fee6aa9f46ac522
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2024-07-23 21:16:32 +0200

    feat: add new settings for databases to tests

--- base/settings/test.py
+++ base/settings/test.py
@@ -3,9 +3,11 @@ from .common import *  # noqa: F403, F401 isort:skip
 
 # DEFAULT_FILE_STORAGE = "base.storages.MediaStorage"
 # STATICFILES_STORAGE = "base.storages.StaticStorage"
-DATABASES["default"] = {
-    "ENGINE": "django.db.backends.sqlite3",
-    "NAME": ":memory:",
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": ":memory:",
+    }
 }
 
 DEBUG = True
