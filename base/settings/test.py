e85f7791862056462c4152e09f178722201961b2
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2024-07-23 20:40:09 +0200

    feat: add sqlite3 to all tests

--- base/settings/test.py
+++ base/settings/test.py
@@ -3,6 +3,10 @@ from .common import *  # noqa: F403, F401 isort:skip
 
 # DEFAULT_FILE_STORAGE = "base.storages.MediaStorage"
 # STATICFILES_STORAGE = "base.storages.StaticStorage"
+DATABASES['default'] = {
+    'ENGINE': 'django.db.backends.sqlite3',
+    'NAME': ':memory:',
+}
 
 DEBUG = True
 LANGUAGE_CODE = "en-us"
