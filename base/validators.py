c77ea90dc6a32a0289a2107dbebe6f8a2b0fd2bf
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2024-08-12 12:42:05 +0200

    feat: update validators

--- base/validators.py
+++ base/validators.py
@@ -3,10 +3,7 @@ from django.utils.translation import gettext_lazy as _
 
 invoice_number_validator = RegexValidator(
     r"^[0-9]+/(0[1-9]|1[0-2])/[0-9]{4}$",
-    _(
-        "Please enter the invoice number in numeric format only, "
-        "following the pattern number/mm/yyyy"
-    ),
+    _("Please  enter the invoice number in the format number/mm/yyyy"),
 )
 
 correction_invoice_number_validator = RegexValidator(
@@ -16,10 +13,7 @@ correction_invoice_number_validator = RegexValidator(
 
 nip_validator = RegexValidator(
     r"^[0-9a-zA-Z]{8,16}$",
-    _(
-        "Please enter the tax ID without special characters "
-        "and with a minimum of 8 characters"
-    ),
+    _("Pleas enter a tax ID with a minimum of 8 characters and no special characters"),
 )
 
 pesel_validator = RegexValidator(
@@ -29,18 +23,17 @@ pesel_validator = RegexValidator(
 
 regon_validator = RegexValidator(
     r"^([0-9]{9,14})$",
-    _("Please enter the REGON using minimum 9 numbers"),
+    _("Please enter the REGON with a minimum 9 numbers"),
 )
 
 currency_validator = RegexValidator(
-    r"^[a-zA-Z]{3}$", _("Please enter the country code using 3 letters")
+    r"^[a-zA-Z]{3}$", _("Please enter the country code with 3 letters")
 )
 
 account_number_validator = RegexValidator(
     r"^[0-9A-Z ]{15,32}$",
     _(
-        "Please enter the account number with a minimum of 15 characters, "
-        "excluding special characters"
+        "Please enter the account number with a minimum of 15 characters and no special characters"
     ),
 )
 
@@ -48,9 +41,9 @@ rate_validator = MaxValueValidator(99)
 
 zip_code_validator = RegexValidator(
     r"^[0-9]{2}-[0-9]{3}$",
-    _("Please enter the zip code using numbers only in the format xx-xxx"),
+    _("Please enter the zip code with a numbers in the format xx-xxx"),
 )
 
 phone_number_validator = RegexValidator(
-    r"^[0-9]{9,}$", _("Please enter a phone using minimum 9 numbers")
+    r"^[0-9]{9,}$", _("Please enter a phone with a minimum 9 numbers")
 )
