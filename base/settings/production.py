b68d87c80c067ca6e409f4170f9bf30e75a834af
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2024-08-28 22:14:03 +0200

    feat: add storages with static files

--- base/settings/production.py
+++ base/settings/production.py
@@ -2,8 +2,15 @@ from .common import *  # noqa: F403, F401 isort:skip
 
 ALLOWED_HOSTS = ["invoice-factory.devsoft.pl"]
 
-DEFAULT_FILE_STORAGE = "base.storages.MediaStorage"
-STATICFILES_STORAGE = "base.storages.StaticStorage"
+
+STORAGES = {
+    "default": {
+        "BACKEND": "base.storages.MediaStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "base.storages.StaticStorage",
+    },
+}
 
 DEBUG = False
 
