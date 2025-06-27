7755a1885f0d15d00ab0052093737c6476dc460d
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2025-06-27 13:09:32 +0200

    refactor: replace direct country retrieval logic with get_user_country_or_404 utility function for improved clarity

---
+++ countries/utils.py
@@ -0,0 +1,10 @@
+from django.http import Http404
+from django.shortcuts import get_object_or_404
+from django.utils.translation import gettext_lazy as _
+from countries.models import Country
+
+def get_user_country_or_404(country_id, user):
+    country = get_object_or_404(Country.objects.select_related("user"), pk=country_id)
+    if country.user != user:
+        raise Http404(_("Country does not exist"))
+    return country
