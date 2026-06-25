d3db0b2e633feaf57f53780e6539b27c4eaa1448
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2026-06-25 16:44:28 +0200

    feat: honeypot and time-based validation have been implemented + tests

--- users/test/test_forms.py
+++ users/test/test_forms.py
@@ -1,4 +1,7 @@
+import time
+
 import pytest
+from django.core.signing import TimestampSigner
 
 from users.factories import UserFactory
 from users.forms import (
@@ -61,8 +64,50 @@ class TestUserCreationForm:
 
     def test_form_fields_have_correct_class(self):
         form = UserCreationForm()
-        for field_name in form.Meta.fields:
+        for field_name in ["email", "password1", "password2"]:
             assert form.fields[field_name].widget.attrs["class"] == "form-control"
+
+    def test_valid_registration(self):
+        timestamp = TimestampSigner().sign(str(time.time() - 5))  # 5 seconds ago
+        form = UserCreationForm(
+            data={
+                "email": "test@test.pl",
+                "password1": "Password123!",
+                "password2": "Password123!",
+                "timestamp": timestamp,
+                "honeypot": "",
+            }
+        )
+        assert form.is_valid()
+
+    def test_honeypot_filled(self):
+        timestamp = TimestampSigner().sign(str(time.time() - 5))
+        form = UserCreationForm(
+            data={
+                "email": "test@test.pl",
+                "password1": "Password123!",
+                "password2": "Password123!",
+                "timestamp": timestamp,
+                "honeypot": "somebotvalue",
+            }
+        )
+        assert not form.is_valid()
+        assert "honeypot" not in form.errors
+        assert "Registration not allowed for bots." in form.non_field_errors()
+
+    def test_too_fast_registration(self):
+        timestamp = TimestampSigner().sign(str(time.time() - 1))  # 1 second ago
+        form = UserCreationForm(
+            data={
+                "email": "test@test.pl",
+                "password1": "Password123!",
+                "password2": "Password123!",
+                "timestamp": timestamp,
+                "honeypot": "",
+            }
+        )
+        assert not form.is_valid()
+        assert "Registration too fast, possible bot." in form.non_field_errors()
 
 
 @pytest.mark.django_db
